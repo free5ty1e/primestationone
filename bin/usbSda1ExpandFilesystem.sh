32c8160a6099c8311f35b6f893d0a507635a00ab
Chris Paiano <chris.paiano@nike.com>
2015-01-26 20:16:11 -0800

    -Now unmounts USB before expanding filesystem

--- bin/usbSda1ExpandFilesystem.sh
+++ bin/usbSda1ExpandFilesystem.sh
@@ -6,6 +6,7 @@ ls /media/usb0
 
 read -p "Press any key to continue auto expanding filesystem on /dev/sda1... " -n1 -s
 
+sudo umount /dev/sda1
 echo Now expanding filesystem to fill USB drive capacity...
 sudo resize2fs -p /dev/sda1
 df -h
