2a1f0374c78f74eb21cb8eb9c9e914f1c86a2892
Chris Paiano <chris.paiano@gmail.com>
2015-01-27 18:46:54 -0800

    -Moved target splash screen version target image to be in home folder instead of creating a file in the primestationone repo folder

--- bin/updateSplashscreenVersion.sh
+++ bin/updateSplashscreenVersion.sh
@@ -4,4 +4,4 @@ primestationVersion=$(cat ~/primestationone/version.txt)
 echo "PrimeStation One $primestationVersion updating splashscreen version text overlay..."
 echo "More info on the process at http://www.instructables.com/id/Add-text-to-images-with-Linux-convert-command/?ALLSTEPS"
 sudo apt-get install -y imagemagick
-convert -pointsize 20 -fill yellow -draw 'text 270,460 "$primestationVersion" ' ~/primestationone/splashscreen.png ~/primestationone/splashscreenversion.png
+convert -pointsize 20 -fill yellow -draw 'text 270,460 "$primestationVersion" ' ~/primestationone/splashscreen.png ~/splashscreenversion.png
