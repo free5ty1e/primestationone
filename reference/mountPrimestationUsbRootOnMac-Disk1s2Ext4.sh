c7c84a4ff949e9eb54881cc56e269e7f85c3f01e
Chris Paiano <chris.paiano@gmail.com>
2015-01-27 18:03:46 -0800

    -Added reference script to use fuse-ext2 on OSX (Mac) to mount the EXT4 root filesystem on the Primestation One USB drive

---
+++ reference/mountPrimestationUsbRootOnMac-Disk1s2Ext4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#install osxfuse with macfuse compatibility option selected, then fuse-ext2 from the web
+mkdir primestationoneroot
+sudo fuse-ext2 /dev/disk1s2 primestationoneroot -o force
+
+
+
+
+
