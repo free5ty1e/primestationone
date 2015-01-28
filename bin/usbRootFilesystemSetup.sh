1d9b063ecf47fb266c5257fadaa910c01d3b7444
Chris Paiano <chris.paiano@nike.com>
2015-01-27 16:42:03 -0800

    -Removed remounting of USB drive before root filesystem transfer in usbRootFilesystemSetup.sh, should fix script (for realizes)

--- bin/usbRootFilesystemSetup.sh
+++ bin/usbRootFilesystemSetup.sh
@@ -22,12 +22,6 @@ sudo mkfs.ext4 /dev/sda1
 echo Check new filesystem...answer yes to any autofix prompts...
 sudo e2fsck /dev/sda1
 
-echo Mounting new USB filesystem...
-sudo mount /dev/sda1 /media/usb0
-
-echo Checking new storage volumes...
-df -h
-
 echo Now replacing fstab and boot cmdline with versions that mount the USB as the root filesystem instead of the 2nd partition of the SD card, which we should be able to safely remove after this is complete...
 sudo cp -v ~/primestationone/reference/etc/fstabForUsb /etc/fstab
 sudo cp -v ~/primestationone/reference/boot/cmdlineForUsb.txt /boot/cmdline.txt
@@ -42,6 +36,9 @@ cowsay -f tux Transferring entire root filesystem from SD to USB!!
 echo Now actually transferring entire root filesystem to USB drive so we can boot from it...using pv with estimate of 4GB to transfer, so do not be alarmed if the progress does not reach 100 percent...
 #sudo dd if=/dev/root of=/dev/sda1 bs=4M
 sudo pv --size 4000000000 /dev/root | sudo dd bs=4M of=/dev/sda1
+
+#echo Checking new root filesystem...
+#sudo e2fsck -f /dev/sda1
 
 usbSda1ExpandFilesystem.sh
 
