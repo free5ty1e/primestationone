4c2a081df8bbe5e03c7f877eaa5a85ed0da905b9
Chris Paiano <chris.paiano@gmail.com>
2015-01-27 19:07:27 -0800

    -Now overlaying actual version # from version.txt in top-right corner (currently right justified manually for an alpha version) in blue text with "convert" command

--- bin/updateSplashscreenVersion.sh
+++ bin/updateSplashscreenVersion.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo apt-get install -y imagemagick
 primestationVersion=$(cat ~/primestationone/version.txt)
 echo "PrimeStation One $primestationVersion updating splashscreen version text overlay..."
 echo "More info on the process at http://www.instructables.com/id/Add-text-to-images-with-Linux-convert-command/?ALLSTEPS"
-sudo apt-get install -y imagemagick
-convert -pointsize 20 -fill yellow -draw 'text 270,460 "$primestationVersion" ' ~/primestationone/splashscreen.png ~/splashscreenversion.png
+echo "......"
+convert -pointsize 42 -fill blue -draw "text 1675,45 \"$primestationVersion\"" ~/primestationone/splashscreen.png ~/splashscreenversion.png
+echo "Complete, if you didn't just see any errors."
