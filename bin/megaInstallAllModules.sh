1d665383aa539b75da5db84dbed8db4fda47a69e
Chris Paiano <chris.paiano@gmail.com>
2015-01-29 22:44:33 -0800

    -Created megaInstalllAllModules.sh script

---
+++ bin/megaInstallAllModules.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cowsay -f hellokitty Installing all mega modules...
+echo =====================> Installing all mega modules...
+
+megaInstallBinsNRoms.sh
+megaInstallBinsNRomsLarge.sh
+megaInstallThemePrimeStationOne.sh
