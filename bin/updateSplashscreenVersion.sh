72c6bf1b87027d2eb053c97f6a560757c8a78d24
Chris Paiano <chris.paiano@gmail.com>
2015-01-31 17:39:35 -0800

    -Version 0.960 alpha

--- bin/updateSplashscreenVersion.sh
+++ bin/updateSplashscreenVersion.sh
@@ -4,5 +4,5 @@ primestationVersion=$(cat ~/primestationone/version.txt)
 echo "PrimeStation One $primestationVersion updating splashscreen version text overlay..."
 echo "More info on the process at http://www.instructables.com/id/Add-text-to-images-with-Linux-convert-command/?ALLSTEPS"
 echo "......"
-convert -pointsize 44 -fill red -draw "text 1675,45 \"$primestationVersion\"" ~/primestationone/splashscreen.png ~/splashscreenversion.png
+convert -pointsize 44 -fill blue -draw "text 1675,45 \"$primestationVersion\"" ~/primestationone/splashscreen.png ~/splashscreenversion.png
 echo "Complete, if you didn't just see any errors."
