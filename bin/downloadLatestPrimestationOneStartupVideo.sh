c6f424913acd59c0a8600611796d001edbf2c045
Chris Paiano <chris.paiano@gmail.com>
2015-02-15 14:19:24 -0800

    -Now trying video.3gp

--- bin/downloadLatestPrimestationOneStartupVideo.sh
+++ bin/downloadLatestPrimestationOneStartupVideo.sh
@@ -4,7 +4,7 @@ echo =====================> Obtaining latest Primestation One startup video..
 
 pushd ~
 rm -f video*
-wget http://verilyshare.circuitstatic.com/video.mp4
 wget http://verilyshare.circuitstatic.com/video.mov
+wget http://verilyshare.circuitstatic.com/video.3gp
 popd
 
