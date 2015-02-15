eab6634a2652fcb6748e458d35933fcb797ca9b3
Chris Paiano <chris.paiano@gmail.com>
2015-02-14 19:33:56 -0800

    -Now ps3 bluetooth legacy mode switch scripts also actually ensure that sixad is set to run on startup with the default settings we just installed...

--- bin/ps3BluetoothDisableLegacyMode.sh
+++ bin/ps3BluetoothDisableLegacyMode.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo cp ~/primestationone/reference/etc/default/sixad /etc/default/sixad
+sudo update-rc.d sixad defaults
--- bin/ps3BluetoothEnableLegacyMode.sh
+++ bin/ps3BluetoothEnableLegacyMode.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo cp ~/primestationone/reference/etc/default/sixad.legacy /etc/default/sixad
+sudo update-rc.d sixad defaults
