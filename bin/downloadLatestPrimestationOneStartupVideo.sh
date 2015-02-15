9b91ff256eef747e1fbf0959e0fb7f41cdafd870
Chris Paiano <chris.paiano@gmail.com>
2015-02-15 14:05:50 -0800

    -Now downloading both video.mp4 and .mov and using both during startup for experimentation purposes

--- bin/downloadLatestPrimestationOneStartupVideo.sh
+++ bin/downloadLatestPrimestationOneStartupVideo.sh
@@ -3,7 +3,8 @@
 echo =====================> Obtaining latest Primestation One startup video..
 
 pushd ~
-rm -f video.mp4
+rm -f video*
 wget http://verilyshare.circuitstatic.com/video.mp4
+wget http://verilyshare.circuitstatic.com/video.mov
 popd
 
