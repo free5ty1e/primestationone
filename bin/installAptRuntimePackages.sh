2db1c5c2e4ab698c8e597c4917f82c2ff9182f73
Chris Paiano <chris.paiano@gmail.com>
2015-02-15 14:24:04 -0800

    -Added mplayer to runtime dependencies to support startup splash video

--- bin/installAptRuntimePackages.sh
+++ bin/installAptRuntimePackages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo =====================> Obtaining runtime packages / dependencies required for the basics for just running PrimeStationOne...
-sudo apt-get install -y git dialog libbluetooth3 bluez bluez-utils bluez-compat bluez-hcidump bluetooth wicd-curses joystick ntfs-3g cowsay imagemagick p7zip
+sudo apt-get install -y git dialog libbluetooth3 bluez bluez-utils bluez-compat bluez-hcidump bluetooth wicd-curses joystick ntfs-3g cowsay imagemagick p7zip mplayer
