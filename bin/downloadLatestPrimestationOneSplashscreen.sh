94bf993a23dd0dc6884b642598006798f6f099af
Chris Paiano <chris.paiano@gmail.com>
2015-02-15 13:32:42 -0800

    -Fixed splash screen download script to remove old splash screens first

--- bin/downloadLatestPrimestationOneSplashscreen.sh
+++ bin/downloadLatestPrimestationOneSplashscreen.sh
@@ -3,6 +3,7 @@
 echo =====================> Obtaining latest Primestation One splashscreen image...
 
 pushd ~
+rm -f splashscreen*
 wget http://verilyshare.circuitstatic.com/splashscreen.png
 popd
 
