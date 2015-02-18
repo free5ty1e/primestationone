eed3d8a4acae7f7caaf3cee8828bee49a50cbc35
Chris Paiano <chris.paiano@gmail.com>
2015-02-18 00:04:46 -0800

    -Created first draft of a reasonable full / first-time PrimeStation One setup / reset script

---
+++ bin/primeStationOneFirstTimeSetupAndReset.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+function pause()
+{
+    read -p "$*"
+}
+
+echo PrimeStationOne Setup script - to be run on the PI after copying the PrimeStationOne files on top of a functional RetroPie image.
+echo Run the primeStationOneCopyFilesToPi script first from the installing computer after setting up an SSH key for passwordless SCP transfers... or just clone the github repo directly onto the Pi, easier...
+
+echo =====================> Making sure things are up to date...
+sudo apt-get -f install
+sudo apt-get -y update
+sudo apt-get -y check
+sudo apt-get -y dist-upgrade
+sudo apt-get -y upgrade
+
+echo =====================> Updating Pi firmware!  If this happens, you probably have to reboot...
+sudo rpi-update
+
+cowsay -f vader Installing PrimeStationOne...
+echo =====================> Beginning install process of custom stuffs and installation of scripts and executables to their correct locations
+~/primestationone/bin/quickUpdatePrimestationOneFiles.sh
+
+echo =====================> Installing corrected blank gamelist.xml files...
+installBlankGamelists.sh
+
+echo =====================> Building hello_pi example and utility projects, because why not...
+pushd /opt/vc/src/hello_pi
+sudo ./rebuild.sh
+popd
+
+echo =====================> Updating PrimestationOne Specific RetroPie packages
+installAllPrimeStationOneEmulatorsFromRetroPie.sh
+
+cowsay Nuking and Fresh-Installing and Updating RetroPie-Setup...
+echo =====================> Now getting the latest RetroPie-Setup script.
+pushd ~
+sudo rm -rf RetroPie-Setup
+git clone --depth=0 git://github.com/petrockblog/RetroPie-Setup.git
+cd ~/RetroPie-Setup
+git pull
+chmod +x retropie_packages.sh
+chmod +x retropie_setup.sh
+popd
+
+cowsay Launching retropie setup menu...
+sudo ~/RetroPie-Setup/retropie_setup.sh
