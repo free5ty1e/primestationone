256060f523989677f149e00d09ced8d061da9efd
Chris Paiano <chris.paiano@gmail.com>
2015-02-18 12:53:38 -0800

    -Created list of libretrocore enabled emulators for display when running the new netplay configuration option in the emulation station menu

---
+++ RetroPie/roms/settings/netplay_configure_enable_disable.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cowsay -f vader Netplay should work for RetroArch / LibRetroCore emulators!  So far should include: FBA, MSX, GameBoy, GameBoy Color, GameGear, Atari Lynx, MAME, TurboGrafx16, Nintendo, Super Nintendo, N64, Sega Master System, Sega Genesis, Sega CD, Sega 32x, Playstation 1, Atari 2600, Atari 7800, Vectrex, 3D0
+netplayConfigForRetroArchLibretrocoreEmulators.sh
