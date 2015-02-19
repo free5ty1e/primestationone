2189840cde0c3fcf5be0ac42d9d23add1e83f27a
Chris Paiano <chris.paiano@gmail.com>
2015-02-18 18:44:52 -0800

    -Created new install windowed mode script that also calls the auto enforce start emulation station and autologin as pi to override the LXDE install's default option of forcing you to start in LXDE...

---
+++ bin/installWindowedModeLxde.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function pause()
+{
+    read -p "$*"
+}
+
+cowsay -f elephant Windowed Mode Startx LXDE...
+echo =====================> Installing Windowed Mode Startx LXDE...
+
+pushd ~
+
+sudo apt-get update
+sudo apt-get -y install lxde
+autoStartEmulationstationEnforce.sh
+cowsay -f vader You should probably reboot before attempting to start windowed mode startx...
+
+popd
+
+pause Really, you should probably reboot before attempting to use windowed mode...
