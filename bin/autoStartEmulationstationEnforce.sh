a074013fd05b282cb3132b314e281522def08320
Chris Paiano <chris.paiano@gmail.com>
2015-02-18 18:45:16 -0800

    -Moar specific message for enforcing auto login and emulation station startup

--- bin/autoStartEmulationstationEnforce.sh
+++ bin/autoStartEmulationstationEnforce.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cowsay -f vader Enforcing emulationstation startup and autologin instead of LXDE...
-user="$SUDO_USER"
+cowsay -f vader Enforcing emulationstation startup and autologin with user Pi instead of LXDE...
 sudo sed /etc/inittab -i -e "s|1:2345:respawn:/sbin/getty --noclear 38400 tty1|1:2345:respawn:\/bin\/login -f pi tty1 \<\/dev\/tty1 \>\/dev\/tty1 2\>\&1|g"
 sudo update-rc.d lightdm disable 2 # taken from /usr/bin/raspi-config
 if ! egrep -i -q "emulationstation$" /etc/profile; then
