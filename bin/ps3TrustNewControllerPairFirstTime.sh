c86b22347f2a10457f42848775ba0add5011dab2
Chris Paiano <chris.paiano@gmail.com>
2015-02-19 22:39:42 -0800

    -Created basic script to attempt to allow user to pair / trust a new PS3 controller for the first time

---
+++ bin/ps3TrustNewControllerPairFirstTime.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cowsay -f calvin Tool to trust a new PS3 controller for the first time...
+sudo bluetoothctl
+agent on
+echo OK, now disconnect your PS3 controller from the USB and press the PS3 button to pair with the Primestation!
+echo When asked to trust the new connection, say yes!
+echo You can then type exit to exit... which I thought was rather clever...
+default-agent
