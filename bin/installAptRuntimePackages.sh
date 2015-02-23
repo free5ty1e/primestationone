21648c6cb98a75f0b2f2b7332b54adcb5c282daa
Chris Paiano <chris.paiano@gmail.com>
2015-02-22 21:40:59 -0800

    -Removed p7zip from prime station runtime packages

--- bin/installAptRuntimePackages.sh
+++ bin/installAptRuntimePackages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo =====================> Obtaining runtime packages / dependencies required for the basics for just running PrimeStationOne...
-sudo apt-get install -y git dialog wicd-curses joystick ntfs-3g cowsay imagemagick p7zip
+sudo apt-get install -y git dialog wicd-curses joystick ntfs-3g cowsay imagemagick
 #libbluetooth3 bluez bluez-utils bluez-compat bluez-hcidump bluetooth
