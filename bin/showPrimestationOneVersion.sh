379ac6cdb747e54d187264bd9f475e5952c32b5f
Chris Paiano <chris.paiano@gmail.com>
2015-02-22 21:00:28 -0800

    -Updated to show cpuspeed during startup script

--- bin/showPrimestationOneVersion.sh
+++ bin/showPrimestationOneVersion.sh
@@ -3,3 +3,5 @@
 primestationVersion=$(cat ~/primestationone/version.txt)
 echo "PrimeStation One $primestationVersion Go!"
 cowsay -f beavis.zen "Heh heh... yeah... PrimeStation One $primestationVersion Go!"
+
+showCpuSpeed.sh
