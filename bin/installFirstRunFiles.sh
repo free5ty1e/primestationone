82c693f7a72ab40e8694dc1f080bae08bf7de5d6
Chris Paiano <chris.paiano@gmail.com>
2015-02-23 18:23:07 -0800

    -Now when resetting, if prime station theme is installed we will reset to that and fall back to simple theme reset if not (better reset behavior)

--- bin/installFirstRunFiles.sh
+++ bin/installFirstRunFiles.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cp -v ~/primestationone/reference/.emulationstation/es_settings.cfg ~/.emulationstation/
+#If primestation theme is installed, reset using that one in the settings.  Otherwise, use simple theme
+if [ -d /etc/emulationstation/themes/primestation ]; then
+    cp -v ~/primestationone/reference/.emulationstation/es_settings.primestationtheme.cfg ~/.emulationstation/es_settings.cfg
+else
+    cp -v ~/primestationone/reference/.emulationstation/es_settings.simpletheme.cfg ~/.emulationstation/es_settings.cfg
+fi
+
 cp -v ~/primestationone/reference/.emulationstation/es_input.cfg ~/.emulationstation/
 cp -v ~/primestationone/reference/.bashrc ~/
 cp -v ~/primestationone/reference/.megarc ~/
