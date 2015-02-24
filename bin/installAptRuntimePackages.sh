7461a643c68f9e970e522404a7d0a5dfe320243f
Chris Paiano <chris.paiano@gmail.com>
2015-02-23 20:13:28 -0800

    -Added angband to the runtime packages because 2.5MB text randomized RPG cmon

--- bin/installAptRuntimePackages.sh
+++ bin/installAptRuntimePackages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo =====================> Obtaining runtime packages / dependencies required for the basics for just running PrimeStationOne...
-sudo apt-get install -y git dialog wicd-curses joystick ntfs-3g cowsay imagemagick
+sudo apt-get install -y git dialog wicd-curses joystick ntfs-3g cowsay imagemagick angband
 #libbluetooth3 bluez bluez-utils bluez-compat bluez-hcidump bluetooth
