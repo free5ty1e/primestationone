4a8bbf1e83c64ab887a56a816201f9268ebc87c2
Chris Paiano <chris.paiano@gmail.com>
2015-02-24 19:46:58 -0800

    -Commented out download of second startup video, only first video for now for testing and debugging

--- bin/downloadLatestPrimestationOneStartupVideo.sh
+++ bin/downloadLatestPrimestationOneStartupVideo.sh
@@ -5,5 +5,5 @@ echo =====================> Obtaining latest Primestation One startup video..
 pushd ~
 rm -f video*
 wget http://verilyshare.circuitstatic.com/video00.mov
-wget http://verilyshare.circuitstatic.com/video01.mov
+#wget http://verilyshare.circuitstatic.com/video01.mov
 popd
