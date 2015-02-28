eeaa690130c660d5237d58a2cc558d5f97e9e0bd
Chris Paiano <chris.paiano@gmail.com>
2015-02-28 10:09:22 -0800

    -Now netplay setup command pulls from centralized list of mapped libretrocore emulators so users know which emulators will work with these controls

--- bin/netplayConfigForRetroArchLibretrocoreEmulators.sh
+++ bin/netplayConfigForRetroArchLibretrocoreEmulators.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cowsay -f vader Netplay should work for RetroArch / LibRetroCore emulators!  So far should include: NeoGeo, MSX, GameBoy, GameBoy Color, GameGear, Atari Lynx, MAME, TurboGrafx16, Nintendo, Super Nintendo, N64, Sega Master System, Sega Genesis, Sega CD, Sega 32x, Playstation 1, Atari 2600, Atari 7800, Vectrex, 3D0
+listOfLibRetroCores=$(cat ~/primestationone/listOfLibRetroCores.txt)
+echo "Netplay $listOfLibRetroCores\n"
+
+cowsay -f vader "Netplay should work for RetroArch / LibRetroCore emulators! $listOfLibRetroCores"
 sudo ~/RetroPie-Setup/retropie_packages.sh retronetplay
