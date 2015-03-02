912a9cb545fa6b39b11039666148f443da0cb758
Chris Paiano <chris.paiano@gmail.com>
2015-03-01 16:01:16 -0800

    -Now splashscreen update will fall back on imgur if verilyshare is down for splashscreen updates

--- bin/downloadLatestPrimestationOneSplashscreen.sh
+++ bin/downloadLatestPrimestationOneSplashscreen.sh
@@ -5,5 +5,11 @@ echo =====================> Obtaining latest Primestation One splashscreen image
 pushd ~
 rm -f splashscreen*
 wget http://verilyshare.circuitstatic.com/splashscreen.png
-popd
 
+#If that didn't work, grab the backup splashscreen from imgur:
+if [ ! -f /tmp/foo.txt ]; then
+    echo "Splashscreen download from verilyshare failed, falling back to imgur backup splashscreen!"
+    wget --output-document=splashscreen.png http://i.imgur.com/UnMdAZX.png
+fi
+
+popd
