4aebd8c35834f3c7e36332ac0015b9005dbf4043
Chris Paiano <chris.paiano@gmail.com>
2015-03-01 16:16:09 -0800

    -Commandline splashscreen quickref command updated

--- bin/splashscreenQuickReference.sh
+++ bin/splashscreenQuickReference.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-#sudo fbi -T 1 -a /home/pi/splashscreenwithcontrolsandversion.png
-fbi -a /home/pi/splashscreenwithcontrolsandversion.png
-sleep 1
+sudo fbi -T 7 -a /home/pi/splashscreenwithcontrolsandversion.png
