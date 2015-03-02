cd5f3c5b17d62a9adb1797b1a95412a309433e8f
Chris Paiano <chris.paiano@gmail.com>
2015-03-01 21:21:29 -0800

    -Now megaInstallAllModules includes new libretrocore binaries module

--- bin/megaInstallAllModules.sh
+++ bin/megaInstallAllModules.sh
@@ -6,6 +6,7 @@ echo =====================> Installing all mega modules and mega tools...
 clearFoldersThatMayHaveOldBinsNRoms.sh
 
 installMegaTools.sh
+megaInstallLibretrocoresBinaries.sh
 megaInstallBinsNRoms.sh
 megaInstallBinsNRomsLarge.sh
 megaInstallThemePrimeStationOne.sh
