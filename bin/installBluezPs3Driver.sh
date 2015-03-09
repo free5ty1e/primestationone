0e847660be5e1066e103960d21d42b9b2c2c5f04
Chris Paiano <chris.paiano@gmail.com>
2015-03-09 15:43:02 -0700

    -Fixed bluez5 build commands & missing dependencies for fresh pi

--- bin/installBluezPs3Driver.sh
+++ bin/installBluezPs3Driver.sh
@@ -3,25 +3,12 @@
 cowsay -f calvin Building Bluez with PS3 sixaxis autopairing driver...
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get -y install libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
+sudo apt-get -y install libusb-dev libdbus-1-dev libglib2.0-dev libusb-dev libdbus-1-dev libglib2.0-dev automake libudev-dev libical-dev libreadline-dev
 
 #wget https://www.kernel.org/pub/linux/bluetooth/bluez-5.19.tar.xz
 #tar xJvf bluez-5.19.tar.xz
 #cd bluez-5.19
 pushd ~
-
-echo Downloading bluez...
-wget https://www.kernel.org/pub/linux/bluetooth/bluez-5.28.tar.xz
-tar xJvf bluez-5.28.tar.xz
-cd bluez-5.28
-
-echo Configuring...
-./configure --prefix=/usr --mandir=/usr/share/man \
---sysconfdir=/etc --localstatedir=/var \
---disable-systemd --enable-sixaxis
-
-echo Compiling...
-make
 
 echo Disabling sixad if installed on startup so the two sixaxis drivers dont interfere...
 sudo update-rc.d -f sixad remove
@@ -29,25 +16,35 @@ sudo update-rc.d -f sixad remove
 echo Removing old Bluez...
 sudo apt-get -y remove --purge bluez
 
-echo Installing new Bluez...
+echo Downloading new bluez 5...
+wget https://www.kernel.org/pub/linux/bluetooth/bluez-5.28.tar.xz
+tar xJvf bluez-5.28.tar.xz
+cd bluez-5.28
+
+echo Configuring new bluez 5...
+./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var --disable-systemd --enable-sixaxis
+
+echo Compiling new bluez 5...
+make
+
+echo Installing new bluez 5...
 sudo make install
 sudo install -v -dm755 /etc/bluetooth
 sudo install -v -m644 src/main.conf /etc/bluetooth/main.conf
 
-echo Installing init script and set to enable at startup...
+echo Installing init script for bluez 5 and set to enable at startup...
 sudo cp -v ~/primestationone/reference/etc/init.d/bluetooth /etc/init.d/
 sudo chmod +x /etc/init.d/bluetooth
 sudo update-rc.d bluetooth defaults
 sudo dpkg --configure -a --force-confold
 
-sudo cat > /etc/udev/rules.d/10-local.rules << _EOF_
-# Set bluetooth power up
-ACTION=="add", KERNEL=="hci0", RUN+="/usr/bin/hciconfig hci0 up"
+sudo -s cat > /etc/udev/rules.d/10-local.rules << _EOF_
+# Set bluetooth power up and p/i scan enabled
+ACTION=="add", KERNEL=="hci0", RUN+="/usr/bin/hciconfig hci0 up piscan"
 _EOF_
 
 sudo /etc/init.d/bluetooth start
 
 ps3TrustUsbControllerForBluezBluetooth.sh
-
 
 popd
