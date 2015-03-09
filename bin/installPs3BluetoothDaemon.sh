582b7cc7fc80dac5abefc35c7814d2b47d3633b8
Chris Paiano <chris.paiano@gmail.com>
2015-03-09 00:52:33 -0700

    -Attempt to fix PS3 qtsixad1.5.1 install script since RetroPie's is busted

--- bin/installPs3BluetoothDaemon.sh
+++ bin/installPs3BluetoothDaemon.sh
@@ -4,13 +4,14 @@ cowsay -f bud-frogs Installing PS3 Bluetooth Daemon...
 echo =====================> Installing Sixad PS3 sixaxis controller bluetooth pairing daemon qtsixad and compatible bluez from apt...
 pushd ~
 sudo apt-get -y install libbluetooth3 bluez bluez-utils bluez-compat bluez-hcidump bluetooth
-tar xfvz QtSixA-1.5.1.tar.gz
-#wget http://sourceforge.net/projects/qtsixa/files/QtSixA%201.5.1/QtSixA-1.5.1-src.tar.gz
-#tar xfvz QtSixA-1.5.1-src.tar.gz
+#tar xfvz QtSixA-1.5.1.tar.gz
+wget http://sourceforge.net/projects/qtsixa/files/QtSixA%201.5.1/QtSixA-1.5.1-src.tar.gz
+tar xfvz QtSixA-1.5.1-src.tar.gz
 cd QtSixA-1.5.1/sixad
 ##wget https://bugs.launchpad.net/qtsixa/+bug/1036744/+attachment/3260906/+files/compilation_sid.patch
 ##patch -Np1 -i compilation_sid.patch
 #patch -Np1 -i ../sixad.pi.patch
+patch -Np1 -i ~/primestationone/reference/qtsixad/compilation_sid.patch
 sudo make
 sudo make install
 sudo mkdir -p /var/lib/sixad/profiles
