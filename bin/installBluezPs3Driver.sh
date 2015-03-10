7b4351ecaed8eda427b63d82f57b7340273807e9
Chris Paiano <chris.paiano@gmail.com>
2015-03-09 21:19:30 -0700

    -Refactor out a variable containing the repeated bluez file name with version, so its easy to change versions -Changed version from 5.28 to 5.27 to match gizmo98's implementation, which should involve autotrusting via info ini file -Fixed autostartup "hciconfig hci0 up piscan" line to actually get inserted without permission issues

--- bin/installBluezPs3Driver.sh
+++ bin/installBluezPs3Driver.sh
@@ -17,34 +17,38 @@ sudo update-rc.d -f sixad remove
 echo Removing old Bluez...
 sudo apt-get -y remove --purge bluez
 
-echo Downloading new bluez 5...
-rm bluez-5.28.tar.xz
-rm -rf bluez-5.28
-wget https://www.kernel.org/pub/linux/bluetooth/bluez-5.28.tar.xz
-tar xJvf bluez-5.28.tar.xz
-cd bluez-5.28
+#BLUEZ_FILE="bluez-5.28"
+#5.28 appears to not want to set the info / trust file up!  Trying 5.27...
+BLUEZ_FILE="bluez-5.27"
 
-echo Configuring new bluez 5...
+echo "Downloading new $BLUEZ_FILE..."
+rm "$BLUEZ_FILE.tar.xz"
+rm -rf "$BLUEZ_FILE"
+wget "https://www.kernel.org/pub/linux/bluetooth/$BLUEZ_FILE.tar.xz"
+tar xJvf "$BLUEZ_FILE.tar.xz"
+cd "$BLUEZ_FILE"
+
+echo "Configuring new $BLUEZ_FILE..."
 sudo ./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var --disable-systemd --enable-sixaxis
 
-echo Compiling new bluez 5...
+echo "Compiling new $BLUEZ_FILE..."
 sudo make
 
-echo Installing new bluez 5...
+echo "Installing new $BLUEZ_FILE..."
 sudo make install
 sudo install -v -dm755 /etc/bluetooth
 sudo install -v -m644 src/main.conf /etc/bluetooth/main.conf
 
-echo Installing init script for bluez 5 and set to enable at startup...
+echo "Installing init script for $BLUEZ_FILE and set to enable at startup..."
 sudo cp -v ~/primestationone/reference/etc/init.d/bluetooth /etc/init.d/
 sudo chmod +x /etc/init.d/bluetooth
 sudo update-rc.d bluetooth defaults
 #sudo dpkg --configure -a --force-confold
 
-sudo -s cat > /etc/udev/rules.d/10-local.rules << _EOF_
+sudo bash -c 'cat > /etc/udev/rules.d/10-local.rules << _EOF_
 # Set bluetooth power up and p/i scan enabled
 ACTION=="add", KERNEL=="hci0", RUN+="/usr/bin/hciconfig hci0 up piscan"
-_EOF_
+_EOF_'
 
 sudo /etc/init.d/bluetooth start
 
