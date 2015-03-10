51c7b5b1857db320bac5d160ad0002718e4e8a75
Chris Paiano <chris.paiano@gmail.com>
2015-03-09 22:11:38 -0700

    -Refined guided ps3 trust script for bluez5, back to 5.28 since now that works

--- bin/ps3TrustUsbControllerForBluezBluetooth.sh
+++ bin/ps3TrustUsbControllerForBluezBluetooth.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-dialog --title 'PS3 Controller Pairing' --msgbox 'Please connect your PS3 controller via USB-CABLE, press PS button and ENTER.' 0 0
+dialog --title 'PS3 Controller Pairing Step 1 of 2' --msgbox 'Please connect your PS3 controller via USB-CABLE, press PS button and ENTER.' 0 0
+#sleep 5
+dialog --title 'PS3 Controller Pairing Step 2 of 2' --msgbox 'Please disconnect your PS3 controller from USB-CABLE, press PS button and ENTER.' 0 0
 # wait 5 seconds so bluez has enough to create directories
 sleep 5
-for file in $(grep -l "Name=PLAYSTATION(R)3 Controller" /var/lib/bluetooth/*/*/info); do
-    sudo sed -i "s/Trusted=false/Trusted=true/g" $file
-done
-/etc/init.d/bluetooth restart
+trustAllEncounteredPs3ControllersBluez5.sh
