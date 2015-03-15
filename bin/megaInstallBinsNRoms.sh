6bd67625aef924137c4bc8637e66c6d6e4e4fb16
Chris Paiano <chris.paiano@gmail.com>
2015-03-14 21:10:26 -0700

    -Updated mega install binsnroms dl link to latest (now with atari800 BIOS and roms, gba bios)

--- bin/megaInstallBinsNRoms.sh
+++ bin/megaInstallBinsNRoms.sh
@@ -6,7 +6,7 @@ echo Installing binsnroms archive...
 pushd ~
 rm binsnroms.7z
 rm -rf binsnroms
-megadl 'https://mega.co.nz/#!ddVVXLQB!CJiHoJDlVIPdyFEuyhs8lmeTmXuCzbzNRMI4zFp6ODw'
+megadl 'https://mega.co.nz/#!5c13CaDQ!i8cqSAZ5WZ9Y_KhZG4VGomzrv8xvqw6_HLbgaUc6sNo'
 echo Extracting 7z archive.....
 7z -d binsnroms.7z
 cd binsnroms
