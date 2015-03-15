94896ff1a100ac80eb56ba1341790570ea1d2527
Chris Paiano <chris.paiano@gmail.com>
2015-03-15 11:49:40 -0700

    -Now theme install script removes old theme before installing fresh

--- bin/installThemePrimestationOne.sh
+++ bin/installThemePrimestationOne.sh
@@ -4,11 +4,19 @@ cowsay -f hellokitty Installing Primestation One theme...
 echo Installing Primestation One theme...
 
 pushd ~
+
+echo Removing old theme files...
+sudo rm -rf /etc/emulationstation/themes/primestation
 rm -rf primestationone-estheme
+
+echo Cloning fresh from github repo...
 git clone https://github.com/free5ty1e/primestationone-estheme.git
 cd primestationone-estheme/
+
+echo Installing primestation one theme...
 ./installToPrimestationOne.sh
 cd ..
+
 echo Cleaning up...
 rm -rf primestationone-estheme
 popd
