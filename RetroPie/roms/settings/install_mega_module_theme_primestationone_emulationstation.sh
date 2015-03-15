b7aeb79180fd80068778dcfd3c952e7475583150
Chris Paiano <chris.paiano@gmail.com>
2015-03-15 12:43:22 -0700

    -Removed primestation theme update from emulationstation settings screen to avoid crash -Enforced removal of this file via installFiles script

--- RetroPie/roms/settings/install_mega_module_theme_primestationone_emulationstation.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-megaInstallThemePrimeStationOne.sh
--- bin/installPrimeStationOneFiles.sh
+++ bin/installPrimeStationOneFiles.sh
@@ -15,6 +15,9 @@ cp -vr .emulationstation/* ~/.emulationstation/
 cp -v .* ~/
 #cp -v *.gz ~/
 
+echo Removing files that should not be there anymore...
+rm ~/RetroPie/roms/settings/install_mega_module_theme_primestationone_emulationstation.sh
+
 #Splashscreen is now taken care of correctly, with the /etc/splashscreen.list file
 #cp -v splashscreen.png ~/RetroPie-Setup/supplementary/splashscreens/retropieproject2014/splashscreen.png
 
