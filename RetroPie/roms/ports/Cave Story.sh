642479c06d9dc6e556e9b5ddd86fec56b3efc97b
Chris Paiano <chris.paiano@gmail.com>
2015-03-14 21:15:03 -0700

    -Updated Cave Story launch script with latest folder structure

--- RetroPie/roms/ports/Cave Story.sh
+++ RetroPie/roms/ports/Cave Story.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/retropie/supplementary/runcommand/runcommand.sh 1 "/opt/retropie/emulators/retroarch/bin/retroarch -L /opt/retropie/libretrocores/cavestory/nxengine_libretro.so --config /opt/retropie/configs/all/retroarch.cfg --appendconfig /opt/retropie/configs/cavestory/retroarch.cfg /opt/retropie/libretrocores/cavestory/datafiles/Doukutsu.exe" "cavestory"
+/opt/retropie/supplementary/runcommand/runcommand.sh 1 "/opt/retropie/emulators/retroarch/bin/retroarch -L /opt/retropie/libretrocores/lr-nxengine/nxengine_libretro.so --config /opt/retropie/configs/cavestory/retroarch.cfg /opt/retropie/libretrocores/lr-nxengine/datafiles/Doukutsu.exe" "lr-nxengine"
