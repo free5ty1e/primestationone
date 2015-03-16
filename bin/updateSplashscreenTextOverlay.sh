19eb9497877539e4e65bc3b4ba093075e33f7936
Chris Paiano <chris.paiano@gmail.com>
2015-03-15 21:00:20 -0700

    -Much faster splashscreen text overlay, 4x faster, single convert command vs previous 4 convert commands -Now splashscreen font all is courier-bold fixed-width font, looks better methinks -Now reallyquickupdate also does reoverlay text on splash

--- bin/updateSplashscreenTextOverlay.sh
+++ bin/updateSplashscreenTextOverlay.sh
@@ -1,40 +1,21 @@
 #!/bin/bash
 
+primestationVersion=$(cat ~/primestationone/reference/txt/version.txt)
+primestationVerColor=$(cat ~/primestationone/reference/txt/vercolor.txt)
 controlsMappingOverlay=$(cat ~/primestationone/reference/txt/controlsMappingOverlay.txt)
 controlsMappingTextColor=$(cat ~/primestationone/reference/txt/controlsMappingTextColor.txt)
-
-echo "PrimeStation One updating splashscreen controls text overlay with"
-echo "$controlsMappingOverlay"
-echo "in color $controlsMappingTextColor..."
-echo "More info on the process at http://www.instructables.com/id/Add-text-to-images-with-Linux-convert-command/?ALLSTEPS"
-echo "......"
-
-convert -pointsize 32 -fill "$controlsMappingTextColor" -draw "text 12,260 \"$controlsMappingOverlay\"" ~/splashscreenversion.png ~/splashscreenwithcontrolsandversiontemp.png
-
-
 listOfLibRetroCores=$(cat ~/primestationone/reference/txt/listOfLibRetroCores.txt)
 colorOfLibRetroCores=$(cat ~/primestationone/reference/txt/listOfLibRetroCoresColor.txt)
-
-echo "PrimeStation One updating splashscreen mapped emulator list text overlay with"
-echo "$listOfLibRetroCores"
-echo "in color $colorOfLibRetroCores..."
-echo "More info on the process at http://www.instructables.com/id/Add-text-to-images-with-Linux-convert-command/?ALLSTEPS"
-echo "......"
-
-convert -pointsize 30 -fill "$colorOfLibRetroCores" -draw "text 1560,260 \"$listOfLibRetroCores\"" ~/splashscreenwithcontrolsandversiontemp.png ~/splashscreenwithcontrolsandversiontemp2.png
-
 keysToQuitEmusList=$(cat ~/primestationone/reference/txt/keysToQuitEmus.txt)
 colorKeysToQuitEmus=$(cat ~/primestationone/reference/txt/keysToQuitEmusColor.txt)
-
-echo "PrimeStation One updating splashscreen non-mapped emulator quit key list text overlay with"
+echo "PrimeStation One $primestationVersion updating splashscreen version text overlay with $primestationVersion in color $primestationVerColor..."
+echo "...and a $controlsMappingTextColor controlsMappingOverlay: "
+echo "$controlsMappingOverlay"
+echo "...and a $colorOfLibRetroCores listOfLibRetroCores: "
+echo "$listOfLibRetroCores"
+echo "...and a $colorKeysToQuitEmus keysToQuitEmusList: "
 echo "$keysToQuitEmusList"
-echo "in color $colorKeysToQuitEmus..."
 echo "More info on the process at http://www.instructables.com/id/Add-text-to-images-with-Linux-convert-command/?ALLSTEPS"
 echo "......"
-
-convert -pointsize 20 -font courier-bold -fill "$colorKeysToQuitEmus" -draw "text 810,245 \"$keysToQuitEmusList\"" ~/splashscreenwithcontrolsandversiontemp2.png ~/splashscreenwithcontrolsandversion.png
-
-rm -v ~/splashscreenwithcontrolsandversiontem*.png
-rm -v ~/splashscreenversion.png
-
+convert -font courier-bold -pointsize 44 -fill "$primestationVerColor" -draw "text 1600,45 \"$primestationVersion\"" -pointsize 32 -fill "$controlsMappingTextColor" -draw "text 12,260 \"$controlsMappingOverlay\"" -pointsize 30 -fill "$colorOfLibRetroCores" -draw "text 1560,260 \"$listOfLibRetroCores\"" -pointsize 20 -fill "$colorKeysToQuitEmus" -draw "text 810,245 \"$keysToQuitEmusList\"" ~/splashscreen.png ~/splashscreenwithcontrolsandversion.png
 echo "Complete, if you didn't just see any errors."
--- bin/updateSplashscreenVersion.sh
+++ bin/updateSplashscreenVersion.sh
@@ -3,15 +3,6 @@
 
 downloadLatestPrimestationOneSplashscreen.sh
 
-primestationVersion=$(cat ~/primestationone/reference/txt/version.txt)
-primestationVerColor=$(cat ~/primestationone/reference/txt/vercolor.txt)
-
-echo "PrimeStation One $primestationVersion updating splashscreen version text overlay with $primestationVersion in color $primestationVerColor..."
-echo "More info on the process at http://www.instructables.com/id/Add-text-to-images-with-Linux-convert-command/?ALLSTEPS"
-echo "......"
-
-convert -pointsize 44 -fill "$primestationVerColor" -draw "text 1660,45 \"$primestationVersion\"" ~/splashscreen.png ~/splashscreenversion.png
-
 updateSplashscreenTextOverlay.sh
 
 downloadLatestPrimestationOneStartupVideo.sh
