35cb63d0574f2189f9b05d15e77e1e0338316510
Chris Paiano <chris.paiano@gmail.com>
2015-03-19 20:11:45 -0700

    -Now quickReset script takes care of the apt fixes and required apt package installs since we really dont want to do that every update...

--- bin/quickResetPrimestationOne.sh
+++ bin/quickResetPrimestationOne.sh
@@ -15,13 +15,20 @@ cowsay -f elephant Installing first run files...
 echo Installing first run files...
 installFirstRunFiles.sh
 
-echo =====================> Installing corrected blank gamelist.xml files...
-installBlankGamelists.sh
+#echo Installing corrected blank gamelist.xml files...
+#installBlankGamelists.sh
 
 clearFoldersThatMayHaveOldScripts.sh
 
 autoStartEmulationstationEnforce.sh
 
 retroPieNukeAndCheckoutFresh.sh
+
+echo Applying various APT fixes just in case there is a problem in the package manager...
+sudo apt-get -fy install
+sudo dpkg --configure -a
+
+echo Ensuring all required apt packages are installed...
+installAptRuntimePackages.sh
 
 quickUpdatePrimestationOneFiles.sh
