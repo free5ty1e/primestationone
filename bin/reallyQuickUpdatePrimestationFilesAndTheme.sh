ba4ff1e0730fc7fe59ff62ae006d2c8d65a6e2c3
Chris Paiano <chris.paiano@gmail.com>
2015-03-19 19:26:59 -0700

    -Fixed reallyQuickUpdate script to utilize git status --porcelain to detect changes upstream quickly

--- bin/reallyQuickUpdatePrimestationFilesAndTheme.sh
+++ bin/reallyQuickUpdatePrimestationFilesAndTheme.sh
@@ -2,10 +2,10 @@
 cowsay -f eyes Initiating a REALLY quick-update of the PrimeStation One and theme...
 echo Initiating a REALLY quick-update of the PrimeStation One and theme...
 pushd ~/primestationone
-if ! git diff-index --quiet HEAD --; then
+if [[ `git status --porcelain` ]]; then
+    echo Changes detected upstream!  Updating...
     git pull
     bin/installPrimeStationOneFiles.sh
-    popd
     updateSplashscreenTextOverlay.sh
 else
     echo No changes exist upstream, no need to perform any update operations for this repo!
