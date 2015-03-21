01d12fd51dd33c3b91c222ab329091b1dc1ff597
Chris Paiano <chris.paiano@gmail.com>
2015-03-21 02:46:04 -0700

    -New mega binsnroms dl link

--- bin/megaInstallBinsNRoms.sh
+++ bin/megaInstallBinsNRoms.sh
@@ -6,7 +6,7 @@ echo Installing binsnroms archive...
 pushd ~
 rm binsnroms.7z
 rm -rf binsnroms
-megadl 'https://mega.co.nz/#!5c13CaDQ!i8cqSAZ5WZ9Y_KhZG4VGomzrv8xvqw6_HLbgaUc6sNo'
+megadl 'https://mega.co.nz/#!5EdTmSDL!j-cfd9HPpwgr9kBQ1_MpBQZQD2QZfxaJDW1Oip0WVsk'
 echo Extracting 7z archive.....
 7z -d binsnroms.7z
 cd binsnroms
