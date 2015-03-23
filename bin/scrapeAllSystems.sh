05f71f2b8dc9ae8dcd12afccce86cbf7776b313c
Chris Paiano <chris.paiano@gmail.com>
2015-03-23 12:48:42 -0700

    -Separate CRC and filename scrapers, crc is still being worked out and currently commented out

--- bin/scrapeAllSystems.sh
+++ bin/scrapeAllSystems.sh
@@ -9,28 +9,38 @@ sudo apt-get -y install python-imaging
 
 pushd ~
 
-echo Cleaning out old manual scraper...
+mkdir scrapers
+cd scrapers
+
+crcScraper="crc-scraper"
+filenameScraper="filename-scraper"
+
+echo Cleaning out old manual filename scraper...
 rm -rf ES-scraper
+rm -rf "$crcScraper"
+rm -rf "$filenameScraper"
 
-echo Cloning new manual scraper...
-git clone https://github.com/thadmiller/ES-scraper.git
 
-cd ES-scraper
+echo Cloning new manual filename based scraper...
+git clone https://github.com/thadmiller/ES-scraper.git "$filenameScraper"
 
-echo Scraping arcade via crc...
+echo Cloning new manual crc based scraper...
+git clone https://github.com/chugcup/ES-scraper.git "$crcScraper"
+
+
 #Remove -l to choose each rom manually
-python scraper.py -pisize -l -crc -rompath /home/pi/RetroPie/roms/mame -name MAMElrmame4all -platform arcade -ext ".zip .ZIP"
 
-echo Scraping arcade via filename...
-python scraper.py -pisize -l -rompath /home/pi/RetroPie/roms/mame -name MAMElrmame4all -platform arcade -ext ".zip .ZIP"
+#echo "Scraping arcade via $crcScraper"
+#python "$crcScraper/scraper.py" -pisize -l -crc -rompath /home/pi/RetroPie/roms/mame -name MAMElrmame4all -platform arcade -ext ".zip .ZIP"
 
+echo "Scraping arcade via $filenameScraper"
+python "$filenameScraper/scraper.py" -pisize -l -rompath /home/pi/RetroPie/roms/mame -name MAMElrmame4all -platform arcade -ext ".zip .ZIP"
 
-echo Scraping nes via crc...
-python scraper.py -pisize -l -crc -rompath /home/pi/RetroPie/roms/nes -name nes -platform nes -ext ".zip .ZIP"
+#echo Scraping nes via crc...
+#python "$crcScraper/scraper.py" -pisize -l -crc -rompath /home/pi/RetroPie/roms/nes -name nes -platform nes -ext ".zip .ZIP"
 
 echo Scraping nes via filename...
-python scraper.py -pisize -l -rompath /home/pi/RetroPie/roms/nes -name nes -platform nes -ext ".zip .ZIP"
-
+python "$filenameScraper/scraper.py" -pisize -l -rompath /home/pi/RetroPie/roms/nes -name nes -platform nes -ext ".zip .ZIP"
 
 echo Complete, hopefully!
 popd
