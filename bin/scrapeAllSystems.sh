8705756b7155b3ecaaf4f40c6ed3d6fe50f73a89
Chris Paiano <chris.paiano@gmail.com>
2015-03-23 11:49:00 -0700

    -Scrapescript update to try arcade and nes with autoclone

--- bin/scrapeAllSystems.sh
+++ bin/scrapeAllSystems.sh
@@ -4,4 +4,26 @@ message="Scraping all systems..."
 echo "$message"
 cowsay -f flaming-sheep "$message"
 
-python scraper.py -pisize -rompath /home/pi/RetroPie/roms/mame -name MAMElrmame4all -platform arcade -ext ".zip .ZIP"
+echo Installing python imaging library PIL for boxart resizing operations...
+sudo apt-get -y install python-imaging"
+
+pushd ~
+
+echo Cleaning out old manual scraper...
+rm -rf ES-scraper
+
+echo Cloning new manual scraper...
+git clone https://github.com/thadmiller/ES-scraper.git
+
+cd ES-scraper
+
+echo Scraping arcade...
+#Remove -l to choose each rom manually
+python scraper.py -pisize -l -rompath /home/pi/RetroPie/roms/mame -name MAMElrmame4all -platform arcade -ext ".zip .ZIP"
+
+echo Scraping nes...
+python scraper.py -pisize -l -rompath /home/pi/RetroPie/roms/nes -name nes -platform nes -ext ".zip .ZIP"
+
+echo Complete, hopefully!
+
+popd
