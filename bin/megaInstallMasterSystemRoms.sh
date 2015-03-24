fc329ad3aed5e2fc23e96546e6f331b3018a053c
Chris Paiano <chris.paiano@gmail.com>
2015-03-24 09:29:51 -0700

    -New Sega Master System roms module, install script, and emulationstation settings mega menu entry

---
+++ bin/megaInstallMasterSystemRoms.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+archivename="megaMasterSystemRoms"
+
+message="Installing $archivename mega module..."
+echo "$message"
+cowsay -f flaming-sheep "$message"
+
+pushd ~
+
+echo Downloading archive from Mega and decompressing it on the fly...
+#tar xvz < <(megadl --path - 'https://mega.co.nz/#!00gBgDrZ!c39s9Zaa1RRdDnULIdx3WG0xqwioE6BybnD99KCfmnw')
+#7z -d < <(megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs')
+#megadl --path - 'https://mega.co.nz/#!00gBgDrZ!c39s9Zaa1RRdDnULIdx3WG0xqwioE6BybnD99KCfmnw' | tar xz
+#megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs' | 7z -d
+
+megadl --no-progress --path=- 'https://mega.co.nz/#!Z05DCbia!Jf6f2TIr6g9TJxJW2JYaEXCpKIKbxsqy20P8VYgPY2c' | tar xj
+
+#rm "$archivename.7z"
+#megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs'
+#echo Extracting 7z archive.....
+#7z -d "$archivename.7z"
+#
+#echo Cleaning up...
+#rm "$archivename.7z"
+
+echo Resetting permissions on roms and BIOS folders...
+sudo chmod -R 777 ~/RetroPie
+
+popd
+echo If you saw any errors, you might consider running a quickUpdatePrimestationOneFiles.sh to ensure you have the latest mega dl link in this script...
