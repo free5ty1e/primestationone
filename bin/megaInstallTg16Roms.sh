66272c6e34aed136b342d40654e27f370dddcb51
Chris Paiano <chris.paiano@gmail.com>
2015-03-23 22:43:41 -0700

    -Now TG16 module downloads and installs on the fly with no archive file saved to disk in between, all future mega rom modules will work like this

--- bin/megaInstallTg16Roms.sh
+++ bin/megaInstallTg16Roms.sh
@@ -7,13 +7,23 @@ echo "$message"
 cowsay -f flaming-sheep "$message"
 
 pushd ~
-rm "$archivename.7z"
-megadl 'https://mega.co.nz/#!FlxCRCDY!U3if2gfTdfNH_jA7MNzqCqlVYmfAIPpDWp_vooOrD8Y'
-echo Extracting 7z archive.....
-7z -d "$archivename.7z"
 
-echo Cleaning up...
-rm "$archivename.7z"
+echo Downloading archive from Mega and decompressing it on the fly...
+#tar xvz < <(megadl --path - 'https://mega.co.nz/#!00gBgDrZ!c39s9Zaa1RRdDnULIdx3WG0xqwioE6BybnD99KCfmnw')
+#7z -d < <(megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs')
+#megadl --path - 'https://mega.co.nz/#!00gBgDrZ!c39s9Zaa1RRdDnULIdx3WG0xqwioE6BybnD99KCfmnw' | tar xz
+#megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs' | 7z -d
+
+megadl --no-progress --path=- 'https://mega.co.nz/#!AtgiBDgZ!oc-s2OYQo444BqcRi-KHH4NthbWRsXHK-r0elNqx96w' | tar xj
+
+#rm "$archivename.7z"
+#megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs'
+#echo Extracting 7z archive.....
+#7z -d "$archivename.7z"
+#
+#echo Cleaning up...
+#rm "$archivename.7z"
+
 echo Resetting permissions on roms and BIOS folders...
 sudo chmod -R 777 ~/RetroPie
 
