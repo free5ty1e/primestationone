7d08a521ca1436bab2b8e79a996c68f0e8b072e4
Chris Paiano <chris.paiano@gmail.com>
2015-03-24 16:09:32 -0700

    -Sega32x mega install script now using pv also

--- bin/megaInstallSega32xRoms.sh
+++ bin/megaInstallSega32xRoms.sh
@@ -9,20 +9,8 @@ cowsay -f flaming-sheep "$message"
 pushd ~
 
 echo Downloading archive from Mega and decompressing it on the fly...
-#tar xvz < <(megadl --path - 'https://mega.co.nz/#!00gBgDrZ!c39s9Zaa1RRdDnULIdx3WG0xqwioE6BybnD99KCfmnw')
-#7z -d < <(megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs')
-#megadl --path - 'https://mega.co.nz/#!00gBgDrZ!c39s9Zaa1RRdDnULIdx3WG0xqwioE6BybnD99KCfmnw' | tar xz
-#megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs' | 7z -d
-
-megadl --no-progress --path=- 'https://mega.co.nz/#!0khDHJBT!q1GDo3pwHNp1_APQk205GWw32mzFsjpTTcs3g22NOL0' | tar xj
-
-#rm "$archivename.7z"
-#megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs'
-#echo Extracting 7z archive.....
-#7z -d "$archivename.7z"
-#
-#echo Cleaning up...
-#rm "$archivename.7z"
+#megadl --no-progress --path=- 'https://mega.co.nz/#!0khDHJBT!q1GDo3pwHNp1_APQk205GWw32mzFsjpTTcs3g22NOL0' | tar xj
+megadl --no-progress --path=- 'https://mega.co.nz/#!0khDHJBT!q1GDo3pwHNp1_APQk205GWw32mzFsjpTTcs3g22NOL0' | pv -p -s 103308384 | tar xvj
 
 echo Resetting permissions on roms and BIOS folders...
 sudo chmod -R 777 ~/RetroPie
