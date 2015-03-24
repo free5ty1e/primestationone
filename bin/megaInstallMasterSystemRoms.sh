1a48267e4b0556d43239cbf66d0b7ae166905354
Chris Paiano <chris.paiano@gmail.com>
2015-03-24 16:22:29 -0700

    -Updated MasterSystem to use PV -Updated MasterSystem megadl link to new archive without bsdtar headers

--- bin/megaInstallMasterSystemRoms.sh
+++ bin/megaInstallMasterSystemRoms.sh
@@ -9,20 +9,7 @@ cowsay -f flaming-sheep "$message"
 pushd ~
 
 echo Downloading archive from Mega and decompressing it on the fly...
-#tar xvz < <(megadl --path - 'https://mega.co.nz/#!00gBgDrZ!c39s9Zaa1RRdDnULIdx3WG0xqwioE6BybnD99KCfmnw')
-#7z -d < <(megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs')
-#megadl --path - 'https://mega.co.nz/#!00gBgDrZ!c39s9Zaa1RRdDnULIdx3WG0xqwioE6BybnD99KCfmnw' | tar xz
-#megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs' | 7z -d
-
-megadl --no-progress --path=- 'https://mega.co.nz/#!Z05DCbia!Jf6f2TIr6g9TJxJW2JYaEXCpKIKbxsqy20P8VYgPY2c' | tar xj
-
-#rm "$archivename.7z"
-#megadl --path - 'https://mega.co.nz/#!YxxQSLoS!sCHIZnsR3ouWfM46Ve30cv3GR6vl7cGz08Zep-e7yvs'
-#echo Extracting 7z archive.....
-#7z -d "$archivename.7z"
-#
-#echo Cleaning up...
-#rm "$archivename.7z"
+megadl --no-progress --path=- 'https://mega.co.nz/#!N4BgXYoQ!5M8r56pUMILBdnyIFnbV6GOf96pURL3bRkp-8SAre98' | pv -p -s 63045779 | tar xvj
 
 echo Resetting permissions on roms and BIOS folders...
 sudo chmod -R 777 ~/RetroPie
