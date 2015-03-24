10ba6f2b2fde69bd0893fe4265169d604f07f0dd
Chris Paiano <chris.paiano@gmail.com>
2015-03-24 16:22:05 -0700

    -Cleanup

--- bin/megaInstallAtari2600Roms.sh
+++ bin/megaInstallAtari2600Roms.sh
@@ -9,7 +9,6 @@ cowsay -f flaming-sheep "$message"
 pushd ~
 
 echo Downloading archive from Mega and decompressing it on the fly...
-#megadl --no-progress --path=- 'https://mega.co.nz/#!0khDHJBT!q1GDo3pwHNp1_APQk205GWw32mzFsjpTTcs3g22NOL0' | tar xj
 megadl --no-progress --path=- 'https://mega.co.nz/#!Zg4CwbyT!4F5mYhMkHwqATZD7lXX3ldm8WWC--qTsoaPXflWG7jM' | pv -p -s 5397363 | tar xvj
 
 echo Resetting permissions on roms and BIOS folders...
--- bin/megaInstallSega32xRoms.sh
+++ bin/megaInstallSega32xRoms.sh
@@ -9,7 +9,6 @@ cowsay -f flaming-sheep "$message"
 pushd ~
 
 echo Downloading archive from Mega and decompressing it on the fly...
-#megadl --no-progress --path=- 'https://mega.co.nz/#!0khDHJBT!q1GDo3pwHNp1_APQk205GWw32mzFsjpTTcs3g22NOL0' | tar xj
 megadl --no-progress --path=- 'https://mega.co.nz/#!0khDHJBT!q1GDo3pwHNp1_APQk205GWw32mzFsjpTTcs3g22NOL0' | pv -p -s 103308384 | tar xvj
 
 echo Resetting permissions on roms and BIOS folders...
