6030c5ce676758183835a0dd15432ae4bfb778bf
Chris Paiano <chris.paiano@gmail.com>
2015-03-25 00:47:03 -0700

    -New mega modules for goodpacks: atari5200, atarilynx, gameboy, gameboycolor, intellvision, vectrex -New emulationstation settings page mega folder entries to install new modules

---
+++ RetroPie/roms/settings/megaModules/install_mega_module_atari5200_roms.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+megaInstallAtari5200Roms.sh
---
+++ bin/megaInstallAtari5200Roms.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+download_install_mega_module_on_the_fly megaAtari5200Roms ~ 1578702 'https://mega.co.nz/#!0k4wTD4J!FnD1Xdu6OKQaFXmgL5VWKWTyMmde9pfBNjqpNECARB0'
+reset_permissions_bios_and_roms
