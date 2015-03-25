ee4eb7f869b54a89d600c421555400e9521c97de
Chris Paiano <chris.paiano@gmail.com>
2015-03-25 09:40:11 -0700

    -New mega install snes module script -New mega install gamegear module script & emulationstation entry

---
+++ bin/megaInstallSnesRoms.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+download_install_mega_module_on_the_fly megaSnesRoms ~ 10603861943 'https://mega.co.nz/#!IhJxXa7S!A2JJnVTJVvIjxv44S-4cgAUUojGVCd0ztPsfnUvJYGc'
+reset_permissions_bios_and_roms
