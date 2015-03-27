632eebee8b2c1673c5fe705efc60f094bbf753bf
Chris Paiano <chris.paiano@gmail.com>
2015-03-27 08:58:53 -0700

    -2g to 4g upgrade script updated to reflect latest differences

--- bin/upgrade2gMinPrimestationToFull.sh
+++ bin/upgrade2gMinPrimestationToFull.sh
@@ -7,14 +7,19 @@ sudo bash -c "echo CONF_SWAPSIZE=100 > /etc/dphys-swapfile"
 cat /etc/dphys-swapfile
 sudo /etc/init.d/dphys-swapfile stop
 sudo /etc/init.d/dphys-swapfile start
-sudo apt-get update
-sudo apt-get -y dist-upgrade
-sudo apt-get -y upgrade
-sudo rm /boot/.firmware_revision
-sudo SKIP_BACKUP=1 rpi-update
+
+#Below is now included in 2G image, no need...
+#sudo apt-get update
+#sudo apt-get -y dist-upgrade
+#sudo apt-get -y upgrade
+#sudo rm /boot/.firmware_revision
+#sudo SKIP_BACKUP=1 rpi-update
+#helloPiBuild.sh
+
+
 installWindowedModeLxde.sh
 installMegaTools.sh
-helloPiBuild.sh
+
 df -h
 
 message="OK... you really should restart now by typing... wait for it... restart"
