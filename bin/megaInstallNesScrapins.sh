adddcdf2065d2a4141dc91b2e9335d0f6af6fe1e
Chris Paiano <chris.paiano@gmail.com>
2015-03-27 12:18:03 -0700

    -New mega module for scraped nes

---
+++ bin/megaInstallNesScrapins.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+download_install_mega_module_on_the_fly megaNesScrapins ~ 153924353 'https://mega.co.nz/#!w1YgmZwD!AdiW0W5pNHR7W4KmHuLGyPYRHfiqJ9hbVxJWigVli44'
+reset_permissions_bios_and_roms
