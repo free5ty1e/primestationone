ed9131614611286f6c7cfc7da37bcab63a85eb90
Chris Paiano <chris.paiano@gmail.com>
2015-03-28 11:22:57 -0700

    -Fixed descent installer to actually unzip untar src first

--- bin/installDescent1and2.sh
+++ bin/installDescent1and2.sh
@@ -14,9 +14,12 @@ echo Descent 1...
 mkdir descent1
 cd descent1
 wget http://www.dxx-rebirth.com/download/dxx/d1x-rebirth_v0.58.1-src.tar.gz
+tar xvzf d1x-rebirth_v0.58.1-src.tar.gz
+cd d1x-rebirth_v0.58.1-src
 wget http://www-user.tu-chemnitz.de/~heinm/tmp/d1x-rebirth-rpi.diff.gz
 zcat d1x-rebirth-rpi.diff.gz | patch -p1
 scons raspberrypi=1
+cd ..
 cd ..
 
 
@@ -24,10 +27,12 @@ echo Descent 2...
 mkdir descent2
 cd descent2
 wget http://www.dxx-rebirth.com/download/dxx/d2x-rebirth_v0.58.1-src.tar.gz
+tar xvzf d2x-rebirth_v0.58.1-src.tar.gz
+cd d2x-rebirth_v0.58.1-src
 wget http://www-user.tu-chemnitz.de/~heinm/tmp/d2x-rebirth-rpi.diff.gz
 zcat d2x-rebirth-rpi.diff.gz | patch -p1
 scons raspberrypi=1
-cd..
-
+cd ..
+cd ..
 
 popd
