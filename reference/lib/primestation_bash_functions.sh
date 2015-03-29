a30d07165efd52d076e739198733296da8a32cce
Chris Paiano <chris.paiano@gmail.com>
2015-03-29 14:37:56 -0700

    -Removed inaccurate info in echo

--- reference/lib/primestation_bash_functions.sh
+++ reference/lib/primestation_bash_functions.sh
@@ -39,7 +39,7 @@ function rsync_with_progress() {
     SOURCES="$1"
     TARGET="$2"
 
-    echo "Executing dry-run to see how many files must be transferred, only for remote rsync operations..."
+    echo "Executing dry-run to see how many files must be transferred..."
     TODO=$(${RSYNC} --dry-run ${RSYNC_ARGS} ${SOURCES} ${TARGET}|grep "^Number of files transferred"|awk '{print $5}')
     #TODO=$(find ${SOURCES} | wc -l)
     echo "$TODO files counted to sync..."
