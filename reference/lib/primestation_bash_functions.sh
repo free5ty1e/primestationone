c9459f72534dbb64c521c985c755310090c34d6b
Chris Paiano <chris.paiano@gmail.com>
2015-03-29 13:40:45 -0700

    -New bash function in lib to rsync with progress

--- reference/lib/primestation_bash_functions.sh
+++ reference/lib/primestation_bash_functions.sh
@@ -30,3 +30,17 @@ function reset_permissions_bios_and_roms() {
     echo Resetting permissions on roms and BIOS folders...
     sudo chmod -R 777 ~/RetroPie
 }
+
+function rsync_with_progress() {
+    RSYNC="ionice -c3 rsync"
+    # don't use --progress
+    RSYNC_ARGS="-xvrltD --delete --stats --human-readable
+    SOURCES="$1"
+    TARGET="$2"
+
+    #echo "Executing dry-run to see how many files must be transferred..."
+    #TODO=$(${RSYNC} --dry-run ${RSYNC_ARGS} ${SOURCES} ${TARGET}|grep "^Number of files transferred"|awk '{print $5}')
+    TODO=$(sudo find ${SOURCES} | wc -l)
+
+    sudo ${RSYNC} ${RSYNC_ARGS} ${SOURCES} ${TARGET} | pv -l -e -p -s "$TODO"
+}
