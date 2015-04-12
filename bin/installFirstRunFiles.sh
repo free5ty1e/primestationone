b8d1b712cb93040c9003cedcb92f9ab89bd4ab4c
Chris Paiano <chris.paiano@gmail.com>
2015-04-12 08:33:08 -0700

    -Now not overwriting retroarch config files during installFirstRunFiles, breaks things... need to set things with iniSet not by replacing files anyway...

--- bin/installFirstRunFiles.sh
+++ bin/installFirstRunFiles.sh
@@ -11,4 +11,4 @@ cp -v ~/primestationone/reference/.emulationstation/es_input.cfg ~/.emulationsta
 cp -v ~/primestationone/reference/.bashrc ~/
 cp -v ~/primestationone/reference/.megarc ~/
 cp -v ~/primestationone/reference/.basilisk_ii_prefs ~/
-sudo cp -vr /home/pi/primestationone/reference/opt/retropie/configs /opt/retropie/
+#sudo cp -vr /home/pi/primestationone/reference/opt/retropie/configs /opt/retropie/
