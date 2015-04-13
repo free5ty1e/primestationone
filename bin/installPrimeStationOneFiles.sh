f87eea8acb807b1b4e1704fa0e4fea1e11838577
Chris Paiano <chris.paiano@gmail.com>
2015-04-12 21:28:37 -0700

    -Now recursively setting executable bit on all scripts in retropie/roms

--- bin/installPrimeStationOneFiles.sh
+++ bin/installPrimeStationOneFiles.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 echo Installing PrimeStation One main files into their proper locations....
-cd ~/primestationone
+pushd ~/primestationone
 sudo cp -v bin/* /usr/local/bin/
 sudo chmod +x /usr/local/bin/*
 sudo cp -vr etc /
 #sudo cp -vr opt /
 sudo cp -vr var /
-cp -vr RetroPie/* ~/RetroPie/
-chmod +x ~/RetroPie/roms/settings/*.sh
 cp -vr .emulationstation/* ~/.emulationstation/
 cp -v .* ~/
+cp -vr RetroPie/* ~/RetroPie/
+
+echo Ensuring executable bits set on all scripts in roms...
+find /home/pi/RetroPie/roms -name '*.sh' -print0 | xargs -0 chmod --verbose 755
 
 echo Removing c64 emulator profile symlink because we want to put configuration files there and have them in the home/pi/.vice folder...
 rm ~/.vice
 mkdir ~/.vice
 cp -vr .vice/* ~/.vice/
 
-cd ~
+popd
