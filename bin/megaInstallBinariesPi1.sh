d3d90003126ff84170eaa07d35dc15c1c3a11251
Chris Paiano <chris.paiano@gmail.com>
2015-04-18 02:41:21 -0700

    -Now clears out old retropie folder before streaming new one in

--- bin/megaInstallBinariesPi1.sh
+++ bin/megaInstallBinariesPi1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Must be run with sudo!!"
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+rm -rf /opt/retropie
 download_install_mega_module_on_the_fly retropie /opt/retropie 214755784 'https://mega.co.nz/#!sJdlFBzT!kaTeZNoQ_I22aeoYiepmbyBB6flNiWZYgUBnviM-Mi4'
 quickCreateFoldersAndLinksAndRemoveOldFiles.sh
 controllerConfigConstruction.sh
