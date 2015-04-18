83149f38cb843fe504b61db9d6d29aa6c13da9d4
Chris Paiano <chris.paiano@gmail.com>
2015-04-17 21:18:38 -0700

    -Now also fixing broken controller configs after retroarch downgrade

--- bin/megaFixRewindWithOlderRetroArch.sh
+++ bin/megaFixRewindWithOlderRetroArch.sh
@@ -3,3 +3,5 @@ echo "Must be run with sudo!!"
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
 download_install_mega_module_on_the_fly retroarch /opt/retropie/emulators/retroarch 2214091 'https://mega.co.nz/#!hQsQBTLa!H7R1_gK8OxzqOLYYByYB4DT7W69vqafiA83qe0pZnb4'
 quickCreateFoldersAndLinksAndRemoveOldFiles.sh
+controllerConfigConstruction.sh
+
