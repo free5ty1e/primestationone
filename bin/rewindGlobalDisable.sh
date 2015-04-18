899f210cc192719aefb4737c8f4bd10627b6e5d6
Chris Paiano <chris.paiano@gmail.com>
2015-04-18 01:30:50 -0700

    -New suite of rewind control scripts to globally disable and enable the rewind feature, and also to separately control the granularity / length of the buffer.

---
+++ bin/rewindGlobalDisable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+message="Disabling global emulation rewind feature which may cause massive slowdown just being on for certain games and emus..."
+echo "$message"
+cowsay -f eyes "$message"
+
+source "/home/pi/RetroPie-Setup/scriptmodules/helpers.sh"
+iniConfig " = " "" "/opt/retropie/configs/all/retroarch.cfg"
+iniSet "rewind_enable" "false"
