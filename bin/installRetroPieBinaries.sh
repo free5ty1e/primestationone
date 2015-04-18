a423125f999579b53554375d4e98f87c0fb83d0c
Chris Paiano <chris.paiano@gmail.com>
2015-04-18 01:35:11 -0700

    -Now enabling coarse global rewind when running the fix rewind scripts...

--- bin/installRetroPieBinaries.sh
+++ bin/installRetroPieBinaries.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 sudo actuallyInstallRetroPieBinaries.sh
+
+echo Now, unless retroarch has fixed their latest versions rewind problem, you will want to run either the megaFixRewindWithOlderRetroArchPi1.sh or the megaFixRewindWithOlderRetroArchPi2.sh script
+echo to fix rewind using a previous retroarch build that is known to work.
