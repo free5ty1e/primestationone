10d44bd3a13a40a2b0c89e58aff14adf5614bf98
Chris Paiano <chris.paiano@gmail.com>
2015-04-19 16:41:45 -0700

    -Fixed incorrect n64 module size for progress

--- bin/megaInstallN64SelectRoms.sh
+++ bin/megaInstallN64SelectRoms.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
-download_install_mega_module_on_the_fly megaN64SelectRoms ~ 916115897 'https://mega.co.nz/#!Z1AESQII!sp8gwt62JHY90PkJIWKVLM59awDurtdBIolHR1Zozsk'
+download_install_mega_module_on_the_fly megaN64SelectRoms ~ 611870089 'https://mega.co.nz/#!Z1AESQII!sp8gwt62JHY90PkJIWKVLM59awDurtdBIolHR1Zozsk'
 reset_permissions_bios_and_roms
