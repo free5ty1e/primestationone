4fcc1e6660537014a384ec6780f0b22ac798f5c1
Chris Paiano <chris.paiano@gmail.com>
2015-04-19 13:17:33 -0700

    -Fixed last modification of /etc/profile during autostart emulationstation enforce script, was failing permissions check

--- bin/autoStartEmulationstationEnforce.sh
+++ bin/autoStartEmulationstationEnforce.sh
@@ -4,5 +4,5 @@ cowsay -f vader Enforcing emulationstation startup and autologin with user Pi in
 sudo sed /etc/inittab -i -e "s|1:2345:respawn:/sbin/getty --noclear 38400 tty1|1:2345:respawn:\/bin\/login -f pi tty1 \<\/dev\/tty1 \>\/dev\/tty1 2\>\&1|g"
 sudo update-rc.d lightdm disable 2 # taken from /usr/bin/raspi-config
 if ! egrep -i -q "emulationstation$" /etc/profile; then
-    echo "[ -n \"\${SSH_CONNECTION}\" ] || emulationstation" >> /etc/profile
+    sudo bash -c "echo [ -n \"\${SSH_CONNECTION}\" ] || emulationstation >> /etc/profile"
 fi
