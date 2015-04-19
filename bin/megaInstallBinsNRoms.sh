2aa3f28afb8f416a6446464a950f91ccbd322470
Chris Paiano <chris.paiano@gmail.com>
2015-04-18 23:46:51 -0700

    -Updated mega modules for binsnroms and binsnroms_large, now complement each other quite nicely for a decent full spread of games for a bit over 3.2GB total between them

--- bin/megaInstallBinsNRoms.sh
+++ bin/megaInstallBinsNRoms.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
-download_install_mega_module_on_the_fly binsnroms ~ 91872109 'https://mega.co.nz/#!B8oDnRIR!TH2bqSgQLB9r4nhL3zz47gM1sW_wsUZXoPifp6f0338'
+download_install_mega_module_on_the_fly binsnroms ~ 110596400 'https://mega.co.nz/#!klJUCLbQ!9EHZ9EyHGByMg-g8G2gJh82U5dQrq6n8yZzCtsyNgYk'
 reset_permissions_bios_and_roms
 
 cd ~/archive
