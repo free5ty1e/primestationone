212241e36067d0ab7a37d677dba243f995ec9936
Chris Paiano <chris.paiano@gmail.com>
2015-04-24 12:31:29 -0700

    -Try3 at this

--- bin/megaCloudBackupSaveStatesAndSrams.sh
+++ bin/megaCloudBackupSaveStatesAndSrams.sh
@@ -4,6 +4,16 @@ CLOUDFOLDER="PrimestationCloud"
 FILENAME="PrimeStationSaveArchive"
 SAVEFILE="/home/pi/$FILENAME"
 
+pushd ~
+
+echo Clearing out any leftover local save archives...
+rm "$SAVEFILE.tar.bz2"
+rm "$SAVEFILE.tar.bz2.bak"
+
+echo Retrieving current cloud save archive to backup before we replace it...
+megaget "/Root/$CLOUDFOLDER/$SAVEFILE.tar.bz2"
+mv "$SAVEFILE.tar.bz2" "$SAVEFILE.tar.bz2.bak"
+
 echo Finding and updating your archove of save states and SRAM files...
 for saveStateFile in /home/pi/RetroPie/roms/*/*.state*; do
     echo "Archiving saveStateFile $saveStateFile..."
@@ -21,14 +31,21 @@ compressBz2.sh "$SAVEFILE.tar"
 echo "Creating cloud folder $CLOUDFOLDER..."
 megamkdir "/Root/$CLOUDFOLDER"
 
-echo Removing existing cloud save archive backup slot...
+echo Removing existing cloud save archive and archive backup slot...
+megarm "/Root/$CLOUDFOLDER/$FILENAME.tar.bz2"
 megarm "/Root/$CLOUDFOLDER/$FILENAME.tar.bz2.bak"
-
-echo Moving existing cloud save archive to backup slot...
-megamv "/Root/$CLOUDFOLDER/$FILENAME.tar.bz2.bak" "/Root/$CLOUDFOLDER/$FILENAME.tar.bz2"
 
 echo Uploading your save archive to your cloud storage...
 megaput --path "/Root/$CLOUDFOLDER" "$SAVEFILE.tar.bz2"
 
+echo Uploading your save archive backup slot to your cloud storage...
+megaput --path "/Root/$CLOUDFOLDER" "$SAVEFILE.tar.bz2.bak"
+
+echo Clearing out any leftover local save archives...
+rm "$SAVEFILE.tar.bz2"
+rm "$SAVEFILE.tar.bz2.bak"
+
 megals /Root/PrimestationCloud
 megadf
+
+popd
