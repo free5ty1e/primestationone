9d4edd0f2587ca32d7bc7e8f8a3e265022762a13
Chris Paiano <chris.paiano@gmail.com>
2015-04-23 20:59:45 -0700

    -try2 at refactoring this out to reusable functions...

--- bin/megaCloudBakLogin.sh
+++ bin/megaCloudBakLogin.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
 
-ask_for_user_input_and_store_result "LOGIN TO MEGA.CO.NZ" --backtitle "EMAIL: For existing users of mega.co.nz" --inputbox "Please enter your email address to login to Mega.co.nz (one that already has an account with them)" 8 60
+ask_for_user_input_and_store_result "LOGIN TO MEGA.CO.NZ" "EMAIL: For existing users of mega.co.nz" "Please enter your email address to login to Mega.co.nz (one that already has an account with them)" 8 60
 email="$RESULT"
 
-ask_for_user_input_and_store_result "LOGIN TO MEGA.CO.NZ" --backtitle "PASSWORD: For existing users of mega.co.nz" --inputbox "Please enter your password to login to Mega.co.nz for user with email $email" 8 60
+ask_for_user_input_and_store_result "LOGIN TO MEGA.CO.NZ" "PASSWORD: For existing users of mega.co.nz" "Please enter your password to login to Mega.co.nz for user with email $email" 8 60
 password="$RESULT"
 
 create_megarc_login_file "$email" "$password"
