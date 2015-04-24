319cea6f4b444a63e777d7b859662a1bfc9f3401
Chris Paiano <chris.paiano@gmail.com>
2015-04-23 19:53:07 -0700

    -Fixing ownership on temp, wtfbbq

--- bin/installPrimeStationOneFiles.sh
+++ bin/installPrimeStationOneFiles.sh
@@ -14,6 +14,9 @@ cp -vr RetroPie/* ~/RetroPie/
 echo Ensuring executable bits set on all scripts in roms...
 find /home/pi/RetroPie/roms -name '*.sh' -print0 | xargs -0 chmod --verbose 755
 
+echo Resetting permissions on sometimes troublesome folders...
+sudo chown pi:pi /home/pi/temp
+
 echo Removing c64 emulator profile symlink because we want to put configuration files there and have them in the home/pi/.vice folder...
 rm ~/.vice
 mkdir ~/.vice
