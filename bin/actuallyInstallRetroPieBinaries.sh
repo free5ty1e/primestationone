de24c74d641c7f718c385630ca4adcdf3eae00e8
Chris Paiano <chris.paiano@gmail.com>
2015-04-26 14:40:04 -0700

    -Now installRetroPieBinaries script also ensures nuke and fresh checkout of RetroPie-Setup to force latest

--- bin/actuallyInstallRetroPieBinaries.sh
+++ bin/actuallyInstallRetroPieBinaries.sh
@@ -2,6 +2,8 @@
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
 fancy_console_message "Updating RetroPie binaries..." "mech-and-cow"
 
+retroPieNukeAndCheckoutFresh.sh
+
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
 prepare_to_directly_run_retropie_script_modules
 
