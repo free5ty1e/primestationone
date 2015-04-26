b524a9742235754768332e58e948e38b74203cff
Chris Paiano <chris.paiano@gmail.com>
2015-04-25 18:41:23 -0700

    -New bluetooth mouse autopairing script, A First Stab by Chris Prime

---
+++ bin/bluetoothPairMouse.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+
+echo Gonna pair the first discoverable bluetooth mouse I find to this Primestation!
+
+echo Stopping sixad service since qtsixad DIRECTLY interferes with the standard bluetooth pairing process...
+sudo service sixad stop
+
+echo Scanning for your mouse... ensure it is discoverable!  Typically done by holding the Pair or Connect button until something flashes blue.  This is the part of the script where your bluetooth mouse should have something flashing blue on it...
+echo Actually scanning twice so you can see the list of discoverable devices for troubleshooting purposes...
+hcitool scan
+firstBluetoothMouseAddress=$(hcitool scan | grep "Mouse" | awk '{print $1}')
+
+if [ -n "${firstBluetoothMouseAddress}" ]; then
+    echo "firstBluetoothMouseAddress located is: $firstBluetoothMouseAddress"
+
+    echo "Initiating pairing procedure between this Primestation and $firstBluetoothMouseAddress..."
+    sudo bluez-simple-agent hci0 "$firstBluetoothMouseAddress"
+
+    echo "Hopefully all went well, I will now set up $firstBluetoothMouseAddress to be trusted and autoconnect whenever the Primestation sees it for uber convenience..."
+    bluez-test-device trusted "$firstBluetoothMouseAddress" yes
+    bluez-test-input connect "$firstBluetoothMouseAddress" yes
+
+else
+    echo No discoverable bluetooth mouse could be found, cannot continue!
+fi
+
+echo Restarting sixad service as we are finished with standard bluetooth pairing operations...
+sudo service sixad start
