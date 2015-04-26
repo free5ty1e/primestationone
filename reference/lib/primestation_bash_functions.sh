b602426c6830dd0a09766b6c6a3bc909afb3f650
Chris Paiano <chris.paiano@gmail.com>
2015-04-26 13:41:30 -0700

    -Refactored out new function in our library to handle preparing to run retropie packages and deep functions directly

--- reference/lib/primestation_bash_functions.sh
+++ reference/lib/primestation_bash_functions.sh
@@ -124,6 +124,55 @@ Password = $password
 _EOF_
 }
 
+function prepare_to_directly_run_retropie_script_modules() {
+    # global variables ==========================================================
+
+    # main retropie install location
+    rootdir="/opt/retropie"
+
+    user="$SUDO_USER"
+    [[ -z "$user" ]] && user=$(id -un)
+
+    home="$(eval echo ~$user)"
+    datadir="$home/RetroPie"
+    biosdir="$datadir/BIOS"
+    romdir="$datadir/roms"
+    emudir="$rootdir/emulators"
+    configdir="$rootdir/configs"
+
+    scriptdir="/home/pi/RetroPie-Setup"
+    __logdir="$scriptdir/logs"
+    __tmpdir="$scriptdir/tmp"
+    __builddir="$__tmpdir/build"
+    __swapdir="$__tmpdir"
+
+    # check, if sudo is used
+    if [[ $(id -u) -ne 0 ]]; then
+    echo "Script must be run as root. Try 'sudo $0'"
+    exit 1
+    fi
+
+    __backtitle="PetRockBlock.com - RetroPie Setup. Installation folder: $rootdir for user $user"
+
+    source "$scriptdir/scriptmodules/system.sh"
+    source "$scriptdir/scriptmodules/helpers.sh"
+    source "$scriptdir/scriptmodules/packages.sh"
+    source "$scriptdir/scriptmodules/admin/setup.sh"
+
+    setup_env
+
+    # set default gcc version
+    gcc_version "$__default_gcc_version"
+
+    mkUserDir "$romdir"
+    mkUserDir "$biosdir"
+
+    rp_registerAllModules
+
+    ensureFBMode 320 240
+
+}
+
 function download_install_mega_archive_from_cloud_storage_on_the_fly() {
     echo "Function download_install_mega_archive_from_cloud_storage_on_the_fly() with parameters:"
     echo "Mega module / archive name: $1"
