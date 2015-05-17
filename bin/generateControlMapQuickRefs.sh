98a73a54ca9e870a4758238fc0f598ca943099b9
Chris Paiano <chris.paiano@gmail.com>
2015-05-17 11:06:19 -0700

    -Fixed usages of new controller overlay generation script

--- bin/generateControlMapQuickRefs.sh
+++ bin/generateControlMapQuickRefs.sh
@@ -2,7 +2,7 @@
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
 
 echo Generating Nintendo64 emulator Mupen64Plus quickref image...
-generateControllerOverlay "~/quickref_n64_mupen64plus.png" "~/primestationone/reference/txt/controlsMappingOverlayN64Mupen64Plus.txt" ""
+generateControllerOverlay "/home/pi/quickref_n64_mupen64plus.png" "/home/pi/primestationone/reference/txt/controlsMappingOverlayN64Mupen64Plus.txt" ""
 
 echo Generating Commodore64 emulator VICE quickref image...
-generateControllerOverlay "~/quickref_c64_vice.png" "~/primestationone/reference/txt/controlsMappingOverlayC64Vice.txt" ""
+generateControllerOverlay "/home/pi/quickref_c64_vice.png" "/home/pi/primestationone/reference/txt/controlsMappingOverlayC64Vice.txt" ""
