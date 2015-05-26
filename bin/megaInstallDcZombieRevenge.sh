2a2beaf03f14b0646829d2af1faf5f4dc0d1c7ce
Chris Paiano <chris.paiano@gmail.com>
2015-05-25 23:30:23 -0700

    -New mega modules for zombie revenge and phantasy star online for dreamcast

---
+++ bin/megaInstallDcZombieRevenge.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+download_install_mega_module_on_the_fly megaDcZombieRevenge ~ 210489084 'https://mega.co.nz/#!9lYX1aCY!WHm0Vd2TDd3gVOnDq4a_VIKGqFFPdE4Zmno2GfoH4ss'
+reset_permissions_bios_and_roms
