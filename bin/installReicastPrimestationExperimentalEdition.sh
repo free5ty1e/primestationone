f6ed93134fa85de18f8423c0bde0e69fb944b99c
Chris Paiano <chris.paiano@gmail.com>
2015-05-31 12:47:55 -0700

    -New reicast experimental edition installer for dev

---
+++ bin/installReicastPrimestationExperimentalEdition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Temporarily modifying RetroPie module..."
+sed -i '/gitPullOrClone/c\gitPullOrClone "$md_build" https://github.com/free5ty1e/reicast-emulator.git free5ty1e/rpi2/merged-impovements' ~/RetroPie-Setup/scriptmodules/emulators/reicast.sh
+
+echo "Requesting a build through official RetroPie calls..."
+sudo ~/RetroPie-Setup/retropie_packages.sh reicast
+
+echo "Restoring original RetroPie-Setup state..."
+pushd ~/RetroPie-Setup
+git reset --hard
+popd
