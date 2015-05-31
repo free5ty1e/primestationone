92bbfadbb91f6b34b18964ca6c51210427782d34
Chris Paiano <chris.paiano@gmail.com>
2015-05-30 23:29:25 -0700

    -New PSX Metal Gear Solid module

---
+++ bin/megaInstallPsxMetalGearSolid.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+download_install_mega_module_on_the_fly megaPsxMetalGearSolid ~ 970582363 'https://mega.co.nz/#!x9YDUSgC!SiQh_nZgLosHhIscgw1goe2pd2ynr7T76jP7IjtrPsI'
+reset_permissions_bios_and_roms
