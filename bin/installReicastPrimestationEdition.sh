d3466f0f3016ed3548cde922f02d7186ea268359
Chris Paiano <chris.paiano@gmail.com>
2015-06-01 18:30:54 -0700

    -Updated reicast primestation edition build repo branches

--- bin/installReicastPrimestationEdition.sh
+++ bin/installReicastPrimestationEdition.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Temporarily modifying RetroPie module..."
-sed -i '/gitPullOrClone/c\gitPullOrClone "$md_build" https://github.com/free5ty1e/reicast-emulator.git free5ty1e/ps3-controller-mappings' ~/RetroPie-Setup/scriptmodules/emulators/reicast.sh
+sed -i '/gitPullOrClone/c\gitPullOrClone "$md_build" https://github.com/free5ty1e/reicast-emulator.git free5ty1e/rpi2/merged-impovements' ~/RetroPie-Setup/scriptmodules/emulators/reicast.sh
 
 echo "Requesting a build through official RetroPie calls..."
 sudo ~/RetroPie-Setup/retropie_packages.sh reicast
