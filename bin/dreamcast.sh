f0dba8815be082a5a540e0821f11b0c4540f9dd9
Chris Paiano <chris.paiano@gmail.com>
2015-06-11 12:49:39 -0700

    -Formatting -Now enforcing pi ownership on vmus

--- bin/dreamcast.sh
+++ bin/dreamcast.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
-
 pushd /opt/retropie/emulators/reicast
-
 echo Reading the entire Reicast emulator into memory to execute from there...
 sudo mkdir tmpfs
 sudo mount -o size=450M -t tmpfs none tmpfs/
 sudo cp -v * tmpfs/
-
-#echo Reading your user VMUs into memory...
-#sudo cp /home/pi/.dcvmu/*.bin .
-
 cd tmpfs
 sudo aoss ./reicast.elf -config config:homedir=/home/pi -config config:image="$1"
 cd ..
-
-#echo Updating VMU units from memory...
-#sudo cp tmpfs/*.bin .
-#mkdir /home/pi/.dcvmu
-#cp *.bin /home/pi/.dcvmu/
-
+echo Ensuring any freshly-created VMUs are owned by pi and not root...
+sudo chown -R pi:pi .reicast
 echo Freeing up memory...
 sudo umount /opt/retropie/emulators/reicast/tmpfs
 sudo rm -rf tmpfs
-
 popd
