ca0a41428241651ff3a239a62b84b0da0058eb0f
Chris Paiano <chris.paiano@gmail.com>
2015-06-10 17:50:03 -0700

    -Python compile?

--- bin/dreamcastMapPs3ControlsForReicast.py
+++ bin/dreamcastMapPs3ControlsForReicast.py
@@ -11,7 +11,7 @@ ps3ControlNamesToConfigureForDreamcast=['PLAYSTATION(R)3 Controller',
 for section in config.sections():
     print "Section: [%s]" % section
     for name in ps3ControlNamesToConfigureForDreamcast:
-        if (section == name)
+        if section == name
             print "[%s] PS3 controller config section discovered!" % (name)
         for options in config.options(section):
             print "%s = %s" % (options, config.get(section, options))
