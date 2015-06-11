19fcb681e1b87308e5ba842d0ebd17066a328749
Chris Paiano <chris.paiano@gmail.com>
2015-06-10 17:45:37 -0700

    -Now attempting to launch python script capable of remapping ps3 controls for dreamcast without causing duplicate issues..

---
+++ bin/dreamcastMapPs3ControlsForReicast.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import ConfigParser
+config = ConfigParser.ConfigParser()
+config.read("/home/pi/.reicast/emu.cfg")
+
+ps3ControlNamesToConfigureForDreamcast=['PLAYSTATION(R)3 Controller',
+                                        'Sony PLAYSTATION(R)3 Controller',
+                                        'Sony Computer Entertainment Wireless Controller']
+
+for section in config.sections():
+    print "Section: [%s]" % section
+    for name in ps3ControlNamesToConfigureForDreamcast:
+        if (section == name)
+            print "[%s] PS3 controller config section discovered!" % (name)
+        for options in config.options(section):
+            print "%s = %s" % (options, config.get(section, options))
