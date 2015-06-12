cc312f323ff946e402a819b3327e4c6c39b20ac9
Chris Paiano <chris.paiano@gmail.com>
2015-06-12 14:55:45 -0700

    -Shrinking Dreamcast tmpfs in launcher to 150MB from 450MB

--- bin/dreamcast.sh
+++ bin/dreamcast.sh
@@ -2,7 +2,7 @@
 pushd /opt/retropie/emulators/reicast
 echo Reading the entire Reicast emulator into memory to execute from there...
 sudo mkdir tmpfs
-sudo mount -o size=450M -t tmpfs none tmpfs/
+sudo mount -o size=150M -t tmpfs none tmpfs/
 sudo cp -v * tmpfs/
 cd tmpfs
 sudo aoss ./reicast.elf -config config:homedir=/home/pi -config config:image="$1"
