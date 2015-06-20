9e5fb2e3ed6c3b3651afb0cec8966fbf2ee61737
Chris Paiano <chris.paiano@gmail.com>
2015-06-19 23:59:24 -0700

    -Now autofixing package mgr issues during apt runtime installs

--- bin/installAptRuntimePackages.sh
+++ bin/installAptRuntimePackages.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo Applying package manager fixes...
+sudo apt-get clean
+sudo dpkg --configure -a
+sudo apt-get -yf install
+
 echo Obtaining runtime packages / dependencies required for the basics for just running PrimeStationOne...
 sudo apt-get install -y git dialog wicd-curses joystick ntfs-3g cowsay imagemagick angband pv bc
 #libbluetooth3 bluez bluez-utils bluez-compat bluez-hcidump bluetooth
