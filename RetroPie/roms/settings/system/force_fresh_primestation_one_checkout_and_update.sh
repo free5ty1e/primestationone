99c6251659bca090af5c2d43b2bf2addaa64564f
Chris Paiano <chris.paiano@gmail.com>
2015-07-10 14:39:28 -0700

    -Try 4 with runcommand wrapper

--- RetroPie/roms/settings/system/force_fresh_primestation_one_checkout_and_update.sh
+++ RetroPie/roms/settings/system/force_fresh_primestation_one_checkout_and_update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-forceFreshCheckoutAndUpdatePrimestationAndRetroPieSetup.sh
+/opt/retropie/supplementary/runcommand/runcommand.sh 0 forceFreshCheckoutAndUpdatePrimestationAndRetroPieSetup.sh
