2a08d99f939e36840e0abbec3f0fa95ad6fbe26d
Chris Paiano <chris.paiano@gmail.com>
2015-08-09 19:18:31 -0700

    -Updated Gran Turismo 2 Arcade Mode PSX to PBP format to save space

--- bin/megaInstallPsxGranTurismo2ArcadeDisc.sh
+++ bin/megaInstallPsxGranTurismo2ArcadeDisc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
-download_install_mega_module_on_the_fly megaPsxGranTurismo2ArcadeDisc ~ 522659321 'https://mega.co.nz/#!ksAwXaQS!JIc8L9ZSUaEdnm7KNC0GqwMxYeOAhbtxUr3tDlhv8ww'
+download_install_mega_module_on_the_fly megaPsxGranTurismo2ArcadeDisc ~ 543276409 'https://mega.co.nz/#!Ms5WxboT!r4Q8TP8v3IwdVmRtoYb4WPh4EtkUgX2nn0h6tCcvU8w'
 reset_permissions_bios_and_roms
