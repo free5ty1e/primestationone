3b7d2b26a9cac8d474d3c3a7e9fdaad442d70f7d
Chris Paiano <chris.paiano@gmail.com>
2015-08-09 18:13:59 -0700

    -New PSX FF7 and MGS megamodules, using new compressed multidisc single file PBP format

--- bin/megaInstallPsxMetalGearSolid.sh
+++ bin/megaInstallPsxMetalGearSolid.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
-download_install_mega_module_on_the_fly megaPsxMetalGearSolid ~ 970582363 'https://mega.co.nz/#!x9YDUSgC!SiQh_nZgLosHhIscgw1goe2pd2ynr7T76jP7IjtrPsI'
+download_install_mega_module_on_the_fly megaPsxMetalGearSolid ~ 968024046 'https://mega.co.nz/#!dp5EkZrI!I4i2WtbYA2Wn4mplBx852G6ZEno3Fj4Xi2bUcUEinvE'
 reset_permissions_bios_and_roms
