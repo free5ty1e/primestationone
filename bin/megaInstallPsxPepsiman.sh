076a24a0350b5c7f97ce52e764c2ded2517ed8f6
Chris Paiano <chris.paiano@gmail.com>
2015-08-10 00:08:38 -0700

    -Updated gran turismo 2 sim mode, metal slug x, pepsiman, pocket fighter, tekken 3 PSX to PBP format to save space

--- bin/megaInstallPsxPepsiman.sh
+++ bin/megaInstallPsxPepsiman.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
-download_install_mega_module_on_the_fly megaPsxPepsiman ~ 142075400 'https://mega.co.nz/#!l1p1FLLA!PiyS8-xbt-zfHbae8tY9DrN0cg4dnVTF1_CDMGxG6a0'
+download_install_mega_module_on_the_fly megaPsxPepsiman ~ 144133008 'https://mega.co.nz/#!spoxHRYK!6nqd_6MW-apRuOPU_ZSNP5MSTJLG4kEkDtoKfuxi84I'
 reset_permissions_bios_and_roms
--- bin/megaInstallPsxTekken3.sh
+++ bin/megaInstallPsxTekken3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
-download_install_mega_module_on_the_fly megaPsxTekken3 ~ 500985527 'https://mega.co.nz/#!JkxEAYoD!xknEWUs6yF7bow5SCvlX-kRgKBXSyMUOBvbAEvyVmTc'
+download_install_mega_module_on_the_fly megaPsxTekken3 ~ 501992175 'https://mega.co.nz/#!ssZEABab!awNVjWOWZn7sr1oVHXz6Rqw6rdNz1xl_TfrkH0m2BbA'
 reset_permissions_bios_and_roms
