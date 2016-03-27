63944cabb4e4f20246bc54ec8b3d91e863725ce0
Chris Paiano <chris.paiano@gmail.com>
2016-03-27 09:53:23 -0700

    bugfix/ffmpeg-installation-procedure-updated-2

--- bin/installRetroArchWithMpegRecordingEnabled.sh
+++ bin/installRetroArchWithMpegRecordingEnabled.sh
@@ -3,10 +3,9 @@
 #sed -i '/gitPullOrClone/c\gitPullOrClone "$md_build" https://github.com/free5ty1e/reicast-emulator.git free5ty1e/rpi2/vmus-in-home-dir' ~/RetroPie-Setup/scriptmodules/emulators/reicast.sh
 
 nukeRetroPieSetupRepoAndCheckoutFresh.sh
-echo "Requesting a build through official RetroPie calls..."
-#PKG_CONFIG_PATH=/opt/ffmpeg/lib/pkgconfig sudo ~/RetroPie-Setup/retropie_packages.sh retroarch
-sudo ~/RetroPie-Setup/retropie_packages.sh retroarch-joyconfig
-sudo ~/RetroPie-Setup/retropie_packages.sh retroarch
+echo "Requesting a build through official RetroPie calls with the FFMPEG location set..."
+sudo ~/RetroPie-Setup/retropie_packages.sh retroarchautoconf
+PKG_CONFIG_PATH=/opt/ffmpeg/lib/pkgconfig sudo ~/RetroPie-Setup/retropie_packages.sh retroarch
 
 #echo "Restoring original RetroPie-Setup state..."
 #pushd ~/RetroPie-Setup
