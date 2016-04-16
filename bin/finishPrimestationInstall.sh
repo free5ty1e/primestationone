759f2618533e44799d21ef034c6937d03372a872
Chris Paiano <chris.paiano@gmail.com>
2016-04-15 17:23:39 -0700

    feature/pi3-fresh-retropie-install-script-updates

--- bin/finishPrimestationInstall.sh
+++ bin/finishPrimestationInstall.sh
@@ -4,10 +4,13 @@
 quickResetPrimestationOne.sh
 installMegaTools.sh
 installReicastPrimestationEdition.sh
-installDescent1and2.sh
+#installDescent1and2.sh
 
-#Ports:
-sudo ~/RetroPie-Setup/retropie_packages.sh darkplaces
+#Setup all available RetroPie binaries:
+sudo ~/RetroPie-Setup/retropie_packages.sh setup binaries
+
+#Built from sources only:
+#   Ports
 sudo ~/RetroPie-Setup/retropie_packages.sh kodi
 sudo ~/RetroPie-Setup/retropie_packages.sh minecraft
 sudo ~/RetroPie-Setup/retropie_packages.sh giana
@@ -16,30 +19,44 @@ sudo ~/RetroPie-Setup/retropie_packages.sh solarus
 sudo ~/RetroPie-Setup/retropie_packages.sh supertux
 sudo ~/RetroPie-Setup/retropie_packages.sh uqm
 sudo ~/RetroPie-Setup/retropie_packages.sh ags
-sudo ~/RetroPie-Setup/retropie_packages.sh openmsx
-sudo ~/RetroPie-Setup/retropie_packages.sh yabause
+#sudo ~/RetroPie-Setup/retropie_packages.sh openmsx
 sudo ~/RetroPie-Setup/retropie_packages.sh wolf4sdl
 sudo ~/RetroPie-Setup/retropie_packages.sh xrick
 sudo ~/RetroPie-Setup/retropie_packages.sh zdoom
 sudo ~/RetroPie-Setup/retropie_packages.sh cannonball
+sudo ~/RetroPie-Setup/retropie_packages.sh px68k
+sudo ~/RetroPie-Setup/retropie_packages.sh coolcv
+sudo ~/RetroPie-Setup/retropie_packages.sh frotz
+sudo ~/RetroPie-Setup/retropie_packages.sh residualvm
+sudo ~/RetroPie-Setup/retropie_packages.sh stratagus
+sudo ~/RetroPie-Setup/retropie_packages.sh zesarux
+sudo ~/RetroPie-Setup/retropie_packages.sh alephone
+sudo ~/RetroPie-Setup/retropie_packages.sh cgenius
+sudo ~/RetroPie-Setup/retropie_packages.sh dxx-rebirth
+sudo ~/RetroPie-Setup/retropie_packages.sh gemrb
+sudo ~/RetroPie-Setup/retropie_packages.sh lincity-ng
+sudo ~/RetroPie-Setup/retropie_packages.sh love
+sudo ~/RetroPie-Setup/retropie_packages.sh openbor
+sudo ~/RetroPie-Setup/retropie_packages.sh openttd
+sudo ~/RetroPie-Setup/retropie_packages.sh opentyrian
+sudo ~/RetroPie-Setup/retropie_packages.sh sdlpop
+sudo ~/RetroPie-Setup/retropie_packages.sh tyrquake
+sudo ~/RetroPie-Setup/retropie_packages.sh uqm
+sudo ~/RetroPie-Setup/retropie_packages.sh zdoom
 
-#Libretrocores:
+#   Libretrocores:
 sudo ~/RetroPie-Setup/retropie_packages.sh lr-4do
 sudo ~/RetroPie-Setup/retropie_packages.sh lr-beetle-vb
-sudo ~/RetroPie-Setup/retropie_packages.sh lr-vba-next
-sudo ~/RetroPie-Setup/retropie_packages.sh lr-ppsspp
-sudo ~/RetroPie-Setup/retropie_packages.sh lr-handy
+sudo ~/RetroPie-Setup/retropie_packages.sh lr-beetle-lynx
+sudo ~/RetroPie-Setup/retropie_packages.sh lr-quicknes
 sudo ~/RetroPie-Setup/retropie_packages.sh lr-virtualjaguar
 sudo ~/RetroPie-Setup/retropie_packages.sh lr-yabause
-sudo ~/RetroPie-Setup/retropie_packages.sh lr-imame4all
-sudo ~/RetroPie-Setup/retropie_packages.sh lr-mame2003
+sudo ~/RetroPie-Setup/retropie_packages.sh lr-beetle-supergrafx
+sudo ~/RetroPie-Setup/retropie_packages.sh lr-desmume
+sudo ~/RetroPie-Setup/retropie_packages.sh lr-gw
 sudo ~/RetroPie-Setup/retropie_packages.sh lr-mame2010
 sudo ~/RetroPie-Setup/retropie_packages.sh lr-mame
-sudo ~/RetroPie-Setup/retropie_packages.sh lr-nxengine
-sudo ~/RetroPie-Setup/retropie_packages.sh lr-o2em
-sudo ~/RetroPie-Setup/retropie_packages.sh lr-pcsx-rearmed
-sudo ~/RetroPie-Setup/retropie_packages.sh lr-prosystem
-sudo ~/RetroPie-Setup/retropie_packages.sh lr-quicknes
+sudo ~/RetroPie-Setup/retropie_packages.sh lr-mess
 
 #TODO: Windowed mode / LXDE
 
