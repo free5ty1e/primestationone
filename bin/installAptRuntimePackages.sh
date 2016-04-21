c117302289f64164b7064d339d4cec7b0e82864d
Chris Paiano <chris.paiano@gmail.com>
2016-04-20 20:05:35 -0700

    feature/ps3-universal-controller-driver-jessie-2

--- bin/installAptRuntimePackages.sh
+++ bin/installAptRuntimePackages.sh
@@ -6,7 +6,7 @@ sudo dpkg --configure -a
 sudo apt-get -yf install
 
 echo Obtaining runtime packages / dependencies required for the basics for just running PrimeStationOne...
-sudo apt-get install -y git dialog joystick ntfs-3g cowsay imagemagick angband pv bc libssl-dev glib-networking
+sudo apt-get install -y git dialog joystick ntfs-3g cowsay imagemagick angband pv bc libssl-dev glib-networking expect
 
 installGoAndAnsize.sh
 #libbluetooth3 bluez bluez-utils bluez-compat bluez-hcidump bluetooth wicd-curses
--- bin/installPs3RetroPieDriverCorrectly.sh
+++ bin/installPs3RetroPieDriverCorrectly.sh
@@ -2,7 +2,8 @@
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
 fancy_console_message "Installing PS3 driver QtSixAd fork with fake controller support and sixpair from sources..." "bud-frogs"
 
-
+sudo killall sixpair
+sudo rm /usr/sbin/sixpair
 sudo killall sixad
 sudo RetroPie-Setup/retropie_packages.sh ps3controller remove
 sudo mkdir -p /var/lib/sixad/profiles
