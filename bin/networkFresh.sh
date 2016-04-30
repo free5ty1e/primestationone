384b0381e1391ff474d1d2d657f75b3a5656fd1b
Circuit Static <circuit@circuitstatic.com>
2016-04-30 00:09:21 -0700

    ooops, typo

--- bin/networkFresh.sh
+++ bin/networkFresh.sh
@@ -17,7 +17,7 @@ fi
 /usr/games/cowsay -f sodomized-sheep "Get rid of wicd crap"
 sudo apt-get remove -y wicd 
 /usr/games/cowsay -f mutilated "Go back to defaults for wireless and other networking"
-sudo apt-get install --reinstall wpasuplicant network-manager
+sudo apt-get install --reinstall wpasupplicant network-manager
 /usr/games/cowsay -f stimpy "Clean up"
 sudo apt-get autoremove -y
 sudo apt-get -y clean
