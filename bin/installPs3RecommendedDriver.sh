53a1a93192a1e1f1e9ed32b3dd1f649c4efe61a1
Chris Paiano <chris.paiano@gmail.com>
2016-09-26 12:37:27 -0700

    +Now just installing the shanwan driver as it appears to be the most friendly / useful here, no longer breaking the ps3 bluetooth driver in Jessie

--- bin/installPs3RecommendedDriver.sh
+++ bin/installPs3RecommendedDriver.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/expect -f
 set timeout 360
 spawn installPs3RetroPieDriverCorrectly.sh
-expect "press the PS button to connect" { send "\r" }
-spawn sudo /home/pi/RetroPie-Setup/retropie_packages.sh ps3controller pair gasia
-expect "press the PS button to connect" { send "\r" }
-spawn sudo /home/pi/RetroPie-Setup/retropie_packages.sh ps3controller pair gasia
-expect "press the PS button to connect" { send "\r" }
+expect "NOTE" { send "\r" }
+# spawn sudo /home/pi/RetroPie-Setup/retropie_packages.sh ps3controller pair gasia
+# expect "press the PS button to connect" { send "\r" }
+# spawn sudo /home/pi/RetroPie-Setup/retropie_packages.sh ps3controller pair gasia
+# expect "press the PS button to connect" { send "\r" }
