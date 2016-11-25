bd4102f32fea52767e3911949a671a785c1e732e
Chris Paiano <chris.paiano@gmail.com>
2016-11-25 12:36:16 -0800

    +Now more forcefully resizing the filesystem using sgdisk commands to ensure the second partition is full-size

--- bin/autoExpandFilesystemNextBoot.sh
+++ bin/autoExpandFilesystemNextBoot.sh
@@ -7,7 +7,8 @@ echo "Installing self-destructing auto-expand filesystem script to run first thi
 # sudo chmod -x /etc/profile.d/01-expand.sh 
 # ls /etc/profile.d -fl
 
-sudo cp -vf /home/pi/primestationone/reference/scripts/resize2fs_once /etc/init.d/
-sudo chmod +x /etc/init.d/resize2fs_once
+# sudo cp -vf /home/pi/primestationone/reference/scripts/resize2fs_once /etc/init.d/
+# sudo chmod +x /etc/init.d/resize2fs_once
+sudo install -m 755 /home/pi/primestationone/reference/scripts/resize2fs_once /etc/init.d/
 # sudo update-rc.d resize2fs_once defaults
 sudo systemctl enable resize2fs_once
