a8c3b1a99f4467ebc038f2cfec3c125071c147e0
Chris Paiano <chris.paiano@gmail.com>
2016-11-26 12:11:15 -0800

    +Stripped Kodi preconfiguration files (over 60MB!!) from repo, oops!  I shoulda made this a mega module. +Converted Kodi preconfiguration package to new Mega module, added install script to menu (+4 squashed commits) Squashed commits: [282db8a] +Oops forgot the reboot after repartitioning [963d813] +Now using raspi-config command to resize the filesystem with a two-stage self-destructive init.d script process, the second part of which the raspi-config handles [ad81e37] +Script to reset / install Kodi configuration / setup from image [e323310] +First stab at an easily-reproducible kodi configuration / setup

---
+++ bin/megaInstallKodiWithExodus.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+download_install_mega_module_on_the_fly megaKodiExodus ~ 47525723 'https://mega.nz/#!lggEAJiQ!DZKQ0T-EnYRjJPWLWHZNLdjs1VUjTQ9hg0trAUeh85s'
+reset_permissions_bios_and_roms
---
+++ bin/resetKodiConfig.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+echo "Configuring Kodi with default setup from image..."
+megaInstallKodiWithExodus.sh
+echo "Complete, if you didn't just see any errors."
