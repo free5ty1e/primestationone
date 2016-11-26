d7a3d8347c740c0d49d4c11968dfe00810873dde
Chris Paiano <chris.paiano@gmail.com>
2016-11-25 16:11:45 -0800

    +Starting work to fix LXDE

--- bin/installWindowedModeLxde.sh
+++ bin/installWindowedModeLxde.sh
@@ -13,7 +13,7 @@ pushd ~
 sudo apt-get update
 sudo apt-get -y install lxde
 autoStartEmulationstationEnforce.sh
-fixStartX.sh
+# fixStartX.sh
 
 cowsay -f vader "You should probably reboot before attempting to start windowed mode startx..."
 
