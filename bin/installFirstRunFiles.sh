ad81e370ebcfb506e9d9cb5a0ea19b74708786ed
Chris Paiano <chris.paiano@gmail.com>
2016-11-26 12:14:42 -0800

    +Script to reset / install Kodi configuration / setup from image

--- bin/installFirstRunFiles.sh
+++ bin/installFirstRunFiles.sh
@@ -7,7 +7,9 @@ else
     cp -v ~/primestationone/reference/.emulationstation/es_settings.simpletheme.cfg ~/.emulationstation/es_settings.cfg
 fi
 
-cp -v ~/primestationone/reference/.emulationstation/es_input.cfg ~/.emulationstation/
 cp -v ~/primestationone/reference/.bashrc ~/
 cp -v ~/primestationone/reference/.basilisk_ii_prefs ~/
+resetEsInputConfig.sh
+resetKodiConfig.sh
+
 #sudo cp -vr /home/pi/primestationone/reference/opt/retropie/configs /opt/retropie/
