963d813097adfdfbddaaaca72f776aa244af27c6
Chris Paiano <chris.paiano@gmail.com>
2016-11-26 20:33:02 -0800

    +Now using raspi-config command to resize the filesystem with a two-stage self-destructive init.d script process, the second part of which the raspi-config handles

--- bin/autoExpandFilesystemNextBoot.sh
+++ bin/autoExpandFilesystemNextBoot.sh
@@ -7,8 +7,6 @@ echo "Installing self-destructing auto-expand MODIFIED filesystem (from a SHRUNK
 # sudo chmod -x /etc/profile.d/01-expand.sh 
 # ls /etc/profile.d -fl
 
-# sudo cp -vf /home/pi/primestationone/reference/scripts/resize2fs_once /etc/init.d/
-# sudo chmod +x /etc/init.d/resize2fs_once
-sudo install -m 755 /home/pi/primestationone/reference/scripts/resize2fs_once /etc/init.d/
-# sudo update-rc.d resize2fs_once defaults
-sudo systemctl enable resize2fs_once
+sudo cp -vf /home/pi/primestationone/reference/scripts/auto_expand_rootfs /etc/init.d/
+sudo chmod +x /etc/init.d/auto_expand_rootfs
+sudo update-rc.d auto_expand_rootfs defaults
