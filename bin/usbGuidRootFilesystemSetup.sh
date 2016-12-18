8e1f2a7a4c3635afec80e9a79ebb387da81079b0
Chris Paiano <chris.paiano@gmail.com>
2016-12-18 01:15:01 -0800

    +Added echo quotes for clarity

--- bin/usbGuidRootFilesystemSetup.sh
+++ bin/usbGuidRootFilesystemSetup.sh
@@ -2,7 +2,7 @@
 
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
 
-cowsay -f eyes Configuring first USB stick to be our new root filesystem!  Using /dev/sda1 with GUID partition table...
+cowsay -f eyes "Configuring first USB stick to be our new root filesystem!  Using /dev/sda1 with GUID partition table..."
 ls /dev/sd*
 df -h
 ls /media/usb0
@@ -10,26 +10,26 @@ ls /media/usb0
 echo "See http://www.raspberrypi.org/forums/viewtopic.php?f=29&t=44177 for more information on this procedure..."
 read -p "MUST BE RUN AS ROOT -- Press any key to continue -- DO NOT RUN THIS IF YOU ALREADY ARE RUNNING YOUR PI ROOT FROM USB! -- or CTRL-C to cancel using /dev/sda1... " -n1 -s
 
-echo Unmounting USB drive, which can be mounted twice in some cases...
+echo "Unmounting USB drive, which can be mounted twice in some cases..."
 umount /dev/sda1
 umount /dev/sda1
 
-echo Installing gdisk to handle GUID partition table initialization...
-apt-get -y install gdisk rsync
+echo "Installing gdisk to handle GUID partition table initialization..."
+apt-get -y install gdisk rsync`
 cleanupTempFiles.sh
 
 #echo Now to zap all partitioning tables currently on the USB drive...
 #echo Type x ENTER to get to the eXperts menu
 #echo Type Z ENTER y ENTER y ENTER to zap all partitioning tables...
 #sudo gdisk /dev/sda
-echo Zapping existing MBR and GPT partition tables on USB drive...
+echo "Zapping existing MBR and GPT partition tables on USB drive..."
 sgdisk -Z /dev/sda
 
 #echo Now we need to create an appropriate partition on the USB drive.
 #echo Type n ENTER ENTER ENTER ENTER ENTER w ENTER y ENTER to write changes...
 #sudo gdisk /dev/sda
 #echo Type i to see info after this to view GUID and note this value... then q to quit once you have it.
-echo Creating new largest possible primary partition on USB drive...
+echo "Creating new largest possible primary partition on USB drive..."
 sgdisk --largest-new=1 /dev/sda
 
 usbRootPartGuid=$(sudo sgdisk -i=1 /dev/sda | grep "Partition unique GUID:" | awk '{print $4}')
@@ -43,41 +43,45 @@ rm /boot/cmdline.txt
 cp /home/pi/cmdline.txt /boot/cmdline.txt
 rm /home/pi/cmdline.txt
 
-echo Ensuring USB drive is unmounted again...
+echo "Ensuring USB drive is unmounted again..."
 umount /dev/sda1
 umount /dev/sda1
 
-echo Now continuing with ext4 filesystem setup and formatting...
+#UNCOMMENT IF ISSUES WITH MKFS BEFORE REBOOTING :o
+#echo "Running partprobe on this device to ensure the kernel has the updated partition table in memory before continuing..."
+#partprobe /dev/sda1
+
+echo "Now continuing with ext4 filesystem setup and formatting..."
 mke2fs -t ext4 -L rootfs /dev/sda1
 
-echo Mounting new USB filesystem...
+echo "Mounting new USB filesystem..."
 mount /dev/sda1 /mnt
 
-echo Transferring root filesystem to USB drive...
+echo "Transferring root filesystem to USB drive..."
 #sudo rsync -axi / /mnt | pv -les $(df -i / | perl -ane 'print $F[2] if $F[5] =/home/pi/ m:^/:') >/dev/null
 rsync_with_progress "/" "/mnt"
 
-echo Now we need to get a unique identifier for the fstab drive information...
-echo Note the Filesystem UUID in the below info for usage in the fstab...
+echo "Now we need to get a unique identifier for the fstab drive information..."
+echo "Note the Filesystem UUID in the below info for usage in the fstab..."
 tune2fs -l /dev/sda1
 usbPart1FilesystemUuid=$(sudo tune2fs -l /dev/sda1 | grep UUID | awk '{print $3}')
 echo "USB Partition 1 Filesystem UUID retrieved: $usbPart1FilesystemUuid"
 
-echo Now modifying /etc/fstab to mount our UUID as the root mount point upon startup...
+echo "Now modifying /etc/fstab to mount our UUID as the root mount point upon startup..."
 sed "s/\${partitionuuid}/$usbPart1FilesystemUuid/" /home/pi/primestationone/reference/etc/fstabForUsbGuid > /home/pi/fstab
 cp /mnt/etc/fstab /home/pi/fstab.bak
 rm /mnt/etc/fstab
 cp /home/pi/fstab /mnt/etc/fstab
 rm /home/pi/fstab
 
-echo Removing USB copyroms service...
+echo "Removing USB copyroms service..."
 rm /mnt/etc/usbmount/mount.d/01_retropie_copyroms
 
-echo Ensuring USB drive is unmounted again...
+echo "Ensuring USB drive is unmounted again..."
 umount /dev/sda1
 umount /dev/sda1
 
-echo Checking new root filesystem...press enter to auto fix any issues that you are prompted for...
+echo "Checking new root filesystem...press enter to auto fix any issues that you are prompted for..."
 e2fsck -f /dev/sda1
 
 #echo Now going to auto expand your USB filesystem to fill the drive.  If you want to manually manage your partitions, or do not want to resize at this time, hit CTRL-C to cancel.
