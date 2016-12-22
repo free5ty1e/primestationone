ac3b4d120471450d038948335777603ebb76e255
Chris Paiano <chris.paiano@gmail.com>
2016-12-22 11:56:54 -0800

    +More sensible defaults for time-lapse capture and construction

--- bin/timelapseIpCamCapture.sh
+++ bin/timelapseIpCamCapture.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-if [ -z "$*" ] ; then 
-	SECONDSPERFRAME=5
-	echo "No delay provided, defaulting to $SECONDSPERFRAME seconds between frames"
-else
-	SECONDSPERFRAME=$1
-	echo "Delay provided: $SECONDSPERFRAME seconds between frames"
-fi
-
-if [ -z "$2" ] ; then
-	URL="http://192.168.1.18:8080/shot.jpg"
-	echo "No URL provided, defaulting to the one at my house: $SNAP"
-else
-	URL="$2"
-	echo "URL provided: $URL"
-fi
-
-if [ -z "$3" ] ; then
+if [ -z "$*" ] ; then
 	OUT=/home/pi/timelapse
 	echo "No output location provided, defaulting to $OUT"
 else
-	OUT="$3"
+	OUT="$1"
 	echo "Output location provided: $OUT"
+fi
+
+if [ -z "$2" ] ; then 
+	SECONDSPERFRAME=5
+	echo "No delay provided, defaulting to $SECONDSPERFRAME seconds between frames"
+else
+	SECONDSPERFRAME=$2
+	echo "Delay provided: $SECONDSPERFRAME seconds between frames"
+fi
+
+if [ -z "$3" ] ; then
+	URL="http://192.168.1.18:8080/shot.jpg"
+	echo "No URL provided, defaulting to the one at my house: $SNAP"
+else
+	URL="$3"
+	echo "URL provided: $URL"
 fi
 
 # NOTE: replace user, password and camera-address with the actual values
