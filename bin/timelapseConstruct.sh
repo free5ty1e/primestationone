93168f859d11f7a5e730381dd3f3b6ddd953bc5e
Chris Paiano <chris.paiano@gmail.com>
2016-12-22 00:50:33 -0800

    +Timelapse scripts updated to take in useful parameters

--- bin/timelapseConstruct.sh
+++ bin/timelapseConstruct.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-OUT=/home/pi/timelapse
-echo "Creating timelapse from $OUT to $OUT/timelapse.mp4 ...."
-# avconv -r 5 -i "$OUT/*.jpg" -vcodec libx264 -r 30 "$OUT/timelapse.mp4"
+if [ -z "$*" ] ; then 
+	TIMELAPSEPHOTOS=/home/pi/timelapse
+	echo "No timelapse photo location provided, defaulting to $TIMELAPSEPHOTOS"
+else
+	TIMELAPSEPHOTOS="$1"
+	echo "Timelapse photo location provided: $TIMELAPSEPHOTOS"
+fi
 
-cat "$OUT/*.jpg" | avconv -r 30 -f image2pipe -codec:v mjpeg -i - -pix_fmt yuvj420p -r 30 -c:v libx264 -y "$OUT/timelapse.mp4"
-# -vf scale=480:300
+if [ -z "$2" ] ; then 
+	OUT="$TIMELAPSEPHOTOS/timelapse.mp4"
+	echo "No timelapse video output location provided, defaulting to $OUT"
+else
+	OUT="$2"
+	echo "Timelapse video output location provided: $OUT"
+fi
 
-echo "Done!"
+echo "Creating timelapse from JPGs in $TIMELAPSEPHOTOS to $TIMELAPSEPHOTOS/timelapse.mp4 ...."
+
+cat "$TIMELAPSEPHOTOS/*.jpg" | avconv -r 30 -f image2pipe -codec:v mjpeg -i - -pix_fmt yuvj420p -r 30 -c:v libx264 -y "$OUT"
+
+echo "Done (as far as you know)!"
--- bin/timelapseIpCamCapture.sh
+++ bin/timelapseIpCamCapture.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+if [ -z "$*" ] ; then 
+	SECONDSPERFRAME=5
+	echo "No delay provided, defaulting to $SECONDSPERFRAME seconds between frames"
+else
+	SECONDSPERFRAME=$1
+	echo "Delay provided: $SECONDSPERFRAME seconds between frames"
+fi
+
+if [ -z "$2" ] ; then
+	URL="http://192.168.1.18:8080/shot.jpg"
+	echo "No URL provided, defaulting to the one at my house: $SNAP"
+else
+	URL="$2"
+	echo "URL provided: $URL"
+fi
+
+if [ -z "$3" ] ; then
+	OUT=/home/pi/timelapse
+	echo "No output location provided, defaulting to $OUT"
+else
+	OUT="$3"
+	echo "Output location provided: $OUT"
+fi
+
 # NOTE: replace user, password and camera-address with the actual values
 # SNAP="wget -nv http://user:password@camera-address/axis-cgi/jpg/image.cgi"
-SNAP="wget -nv http://192.168.1.18:8080/shot.jpg"
-OUT=/home/pi/timelapse
-SECONDSPERFRAME=5
+SNAP="wget -nv $URL"
 mkdir $OUT
 echo "Now capturing $SNAP to $OUT, press CTRL-C to stop capturing!" 
 while true; do
@@ -13,32 +35,3 @@ while true; do
         echo "Waiting $SECONDSPERFRAME before next capture..."
         sleep $SECONDSPERFRAME
 done
-
-
-# # Timelapse controller for USB webcam
-
-# DIR=frames
-
-# #counter for filename
-# n=1
-# #counter for files saved
-# x=1
-# #number of screenshots to take
-# num=10000
-# #interval at which screenshots get taken in seconds
-# interval=5
-# while [ $x -le $num ]; do
-
-# filename=$(printf "%05d.jpg" "$n")
-# let n=n+1
-
-# #Capture image
-# fswebcam -d /dev/video0 -S 5 -D 1 -r 1280x720 --jpeg 95 --no-banner --save $DIR/$filename
-# #add timestamp with imagemagick
-# convert $DIR/$filename -pointsize 30 -fill white -stroke black -gravity southeast -annotate +4+0 "$(date +"%D %r")" $DIR/$filename
-
-# x=$(( $x + 1 ))
-
-# sleep $interval;
-
-# done;
