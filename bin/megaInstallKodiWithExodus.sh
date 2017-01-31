18b492b5139f4a5d2e91a71da315e21477c4d8a8
Chris Paiano <chris.paiano@gmail.com>
2017-01-31 00:54:18 -0800

    +Cleaned up && updated kodi exodus mega module

--- bin/megaInstallKodiWithExodus.sh
+++ bin/megaInstallKodiWithExodus.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
-download_install_mega_module_on_the_fly megaKodiExodus ~ 57179365 'https://mega.co.nz/#!J9ZQ0boA!fdJep_T9_-NKw2FwUD8Ockg7BiQGas458gq6HKOcbzA'
+download_install_mega_module_on_the_fly megaKodiExodus ~ 50392702 'https://mega.co.nz/#!co5wBAgS!mP0r9qSiVmDLYaWLrr1j_G9YYEaQGrq3ad_DkEU93ek'
 reset_permissions_bios_and_roms
