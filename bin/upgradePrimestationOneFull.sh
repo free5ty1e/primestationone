8cc9a346abaf79716929758a15e4e942eabbd405
Chris Paiano <chris.paiano@gmail.com>
2017-02-06 21:39:14 -0800

    +Script to actually upgrade a Wheezy Primestation to a Jessie Primestation :o (maybe)

--- bin/upgradePrimestationOneFull.sh
+++ bin/upgradePrimestationOneFull.sh
@@ -2,6 +2,28 @@
 
 pushd ~
 
+echo "Running a cloud backup before upgrading..."
+megaCloudSyncSaveStatesAndSrams.sh
+
+echo "Making some room... you may have to reinstall some mega modules..."
+rm -rfv ~/RetroPie/roms/segacd/*
+rm -rfv ~/RetroPie/roms/dreamcast/P*
+rm -rfv ~/RetroPie/roms/msx/*
+
+echo "Upgrading Wheezy to the latest first if we are still on wheezy..."
+sudo apt-get update
+sudo apt-get -yf upgrade
+sudo apt-get -yf dist-upgrade
+
+echo "Changing apt sources to point to jessie instead of wheezy..."
+sudo sed -i 's/wheezy/jessie/g' /etc/apt/sources.list
+sudo sed -i 's/deb http:\/\/archive.raspberrypi.org\/debian jessie main/#deb http:\/\/archive.raspberrypi.org\/debian jessie main/g' /etc/apt/sources.list
+
+echo "Upgrading to Raspbian Jessie..."
+sudo apt-get update
+sudo apt-get -yf upgrade
+sudo apt-get -yf dist-upgrade
+
 nukeRetroPieSetupRepoAndCheckoutFresh.sh
 
 sudo RetroPie-Setup/retropie_packages.sh raspbiantools apt_upgrade
