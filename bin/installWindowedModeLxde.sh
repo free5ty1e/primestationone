f3641a52aa5bc6346809be9ec5b8a14fcc67f329
Chris Paiano <chris.paiano@gmail.com>
2018-04-10 15:06:29 -0700

    Updated install Windowed mode script to manually install the raspbian packages for the default desktop

--- bin/installWindowedModeLxde.sh
+++ bin/installWindowedModeLxde.sh
@@ -10,28 +10,30 @@ echo "Installing Windowed Mode Startx LXDE..."
 #autoStartEmulationstationEnforce.sh
 # fixStartX.sh
 
-sudo ~/RetroPie-Setup/retropie_packages.sh raspbiantools lxde
+# sudo ~/RetroPie-Setup/retropie_packages.sh raspbiantools lxde
+sudo apt-get install --no-install-recommends lxde
+sudo apt-get install xorg raspberrypi-ui-mods rpi-chromium-mods
 
-echo "Adding latest mono-xsp4 key server to repositories and performing update && upgrade to enable install of mono v4+ (see http://www.mono-project.com/docs/getting-started/install/linux/ )..."
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-sudo apt-get update
-sudo apt-get -y upgrade
-echo "Installing prerequisites in order for mono-xsp4..."
-sudo apt-get install -y libmono-system-web4.0-cil
-sudo apt-get install -y mono-devel
-echo "Actually installing mono-xsp4..."
-sudo apt-get -y install mono-xsp4
+# echo "Adding latest mono-xsp4 key server to repositories and performing update && upgrade to enable install of mono v4+ (see http://www.mono-project.com/docs/getting-started/install/linux/ )..."
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+# echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+# sudo apt-get update
+# sudo apt-get -y upgrade
+# echo "Installing prerequisites in order for mono-xsp4..."
+# sudo apt-get install -y libmono-system-web4.0-cil
+# sudo apt-get install -y mono-devel
+# echo "Actually installing mono-xsp4..."
+# sudo apt-get -y install mono-xsp4
 
-echo "Installing and enabling RealVNC server and some other handy gui packages..."
-sudo apt-get -y install realvnc-vnc-server gedit
+# echo "Installing and enabling RealVNC server and some other handy gui packages..."
+# sudo apt-get -y install realvnc-vnc-server gedit
 
-sudo systemctl enable vncserver-x11-serviced.service
-sudo systemctl start vncserver-x11-serviced.service
+# sudo systemctl enable vncserver-x11-serviced.service
+# sudo systemctl start vncserver-x11-serviced.service
 
-echo "Enabling virtual RealVNC server too..."
-sudo systemctl enable vncserver-virtuald.service
-sudo systemctl start vncserver-virtuald.service
+# echo "Enabling virtual RealVNC server too..."
+# sudo systemctl enable vncserver-virtuald.service
+# sudo systemctl start vncserver-virtuald.service
 
 echo "Setting up new EmulationStation entry for Desktop..."
 mkdir /home/pi/RetroPie/roms/desktop
