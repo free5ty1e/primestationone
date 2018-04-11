ca15dd57659735adb43d2992c1b70b4631c3160b
Chris Paiano <chris.paiano@gmail.com>
2018-04-10 18:33:10 -0700

    Added cleanup step to mega DoomWads module

--- bin/megaInstallDoomWads.sh
+++ bin/megaInstallDoomWads.sh
@@ -2,3 +2,5 @@
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
 download_install_mega_module_on_the_fly megaDoomWads ~ 101694852 'https://mega.co.nz/#!otxVxDoR!VrRoflAkl2wll4pXM3onfhOLHLLLILRbsYy0D6IIu-w'
 reset_permissions_bios_and_roms
+
+rm ./RetroPie/roms/ports/._*
