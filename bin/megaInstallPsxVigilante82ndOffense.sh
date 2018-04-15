bced6b95f49604d9ed7c02f47fd7a4df9050526a
Chris Paiano <chris.paiano@gmail.com>
2018-04-14 20:52:39 -0700

    New PSX Mega modules

---
+++ bin/megaInstallPsxVigilante82ndOffense.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+download_install_mega_module_on_the_fly megaPsxVigilante82ndOffense ~ 615824337 'https://mega.co.nz/#!Oy5WEaLS!AgcO5w6XM2Lf5WUfdAoWQphUlOKbJ-w3R2SLk-yNIFo'
+reset_permissions_bios_and_roms
