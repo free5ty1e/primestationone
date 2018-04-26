df5cd3897753978c9c9ebc79aa9666803dcde1f9
Chris Paiano <chris.paiano@gmail.com>
2018-04-26 15:31:33 -0700

    Now pointing to new RetroPie repo from nuke script

--- bin/nukeRetroPieSetupRepoAndCheckoutFresh.sh
+++ bin/nukeRetroPieSetupRepoAndCheckoutFresh.sh
@@ -10,7 +10,8 @@ echo Now getting the latest RetroPie-Setup script.
 pushd ~
 sudo rm -rf RetroPie-Setup
 #git clone --depth=0 git://github.com/petrockblog/RetroPie-Setup.git
-git clone --recursive git://github.com/petrockblog/RetroPie-Setup.git
+# git clone --recursive git://github.com/RetroPie/RetroPie-Setup.git
+git clone --depth=1 git://github.com/RetroPie/RetroPie-Setup.git
 cd ~/RetroPie-Setup
 git pull
 chmod +x retropie_packages.sh
