a3b1bbffac1cffacb0111b472457bc74df2e8a50
Chris Paiano <chris.paiano@gmail.com>
2018-04-30 13:00:55 -0700

    Updated killall script

--- bin/killAllEmusAndRestartEmulationStation.sh
+++ bin/killAllEmusAndRestartEmulationStation.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 echo "Killing all emulators and restarting emulationstation -- Panic Button!"
 
-killall mupen64plus ; killall emulationstation ; killall retroarch ; killall reicast ; emulationstation 2>&1 > /dev/tty1 &
+killall mupen64plus
+killall emulationstation
+killall retroarch
+killall reicast
+killall kodi_v7.bin
+killall xroar
+killall vice
+
+emulationstation 2>&1 > /dev/tty1 &
