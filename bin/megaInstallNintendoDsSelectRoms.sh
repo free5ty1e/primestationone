17dcad4c8defbc4e42b589780101039e8342dac7
Chris Paiano <chris.paiano@gmail.com>
2018-04-30 13:02:19 -0700

    New Mega module

---
+++ bin/megaInstallNintendoDsSelectRoms.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+download_install_mega_module_on_the_fly megaNintendoDsSelectRoms ~ 3354395189 'https://mega.co.nz/#!LrYHxRDY!XB80O-wn78OruJfstzBU4-1r6VnIXNH24E8HyAdc1ME'
+reset_permissions_bios_and_roms
