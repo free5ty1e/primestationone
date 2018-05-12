cf011b3d221b2e3aebad1adce2f02c53dc8f2e01
Chris Paiano <chris.paiano@gmail.com>
2018-05-12 00:59:39 -0700

    New mega module

---
+++ RetroPie/roms/settings/megaModules/install_mega_module_apple2_roms.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+megaInstallApple2Roms.sh
---
+++ bin/megaInstallApple2Roms.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+download_install_mega_module_on_the_fly megaApple2Roms ~ 465017851 'https://mega.co.nz/#!X74zlD5C!bLp9NxCKACsK3epbvkQ7ZUm0GBkWJI2L1VHXZIqWkgo'
+reset_permissions_bios_and_roms
