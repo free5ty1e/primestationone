3b12b3d91fe6a3149260895a3a173f9bfada934b
Chris Paiano <chris.paiano@gmail.com>
2018-05-24 21:59:26 -0700

    Updated ps3 driver install script to add a sleep to the rc.local modifications to allow time for bluetooth to start up, thereby allowing the auto-pairing / trusting agent to actually execute during rc.local

--- bin/installPs3RecommendedDriver.sh
+++ bin/installPs3RecommendedDriver.sh
@@ -3,21 +3,24 @@
 # Install bluez5 auto pairing capabilities:
 sudo sed -i -- 's/#deb-src/deb-src/g' /etc/apt/sources.list && sudo sed -i -- 's/# deb-src/deb-src/g' /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get -y install libbluetooth-dev build-dep bluez-tools
-pip install wheel
-pip install pybluez
+sudo apt-get -y install libbluetooth-dev bluez-tools
+sudo -H pip install wheel
+sudo -H pip install pybluez
 wget https://raw.githubusercontent.com/pauloborges/bluez/master/test/bluezutils.py
 sudo cp -vf bluezutils.py /usr/local/bin/
+
+# grep -q -F 'auto-agent.py &' /home/pi/.bashrc || echo 'auto-agent.py &' >> /home/pi/.bashrc
+# cat /home/pi/.bashrc
 
 if [ "`tail -n1 /etc/rc.local`" != "exit 0" ]; then
 	echo "Last line of rc.local is not exit 0... not modifying rc.local for now..."
 #TODO: find exit 0 and insert line before it...
 else
 	echo "Last line of rc.local is exit 0, modifying line before last..."
-	sudo sed -i -e '$i \auto-agent.py &' /etc/rc.local 
-
+	sudo sed -i -e '$i \sleep 10 && auto-agent.py &' /etc/rc.local 
 fi
 cat /etc/rc.local
+sudo chmod +x /etc/rc.local
 
 
 
