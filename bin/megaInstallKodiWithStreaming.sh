3ae523cfcf7632212b97cc7caf1a2796f449aed0
Chris Paiano <chris.paiano@gmail.com>
2018-05-25 00:49:50 -0700

    Updated kodi megamodule with placenta and posiedon rising

--- bin/megaInstallKodiWithStreaming.sh
+++ bin/megaInstallKodiWithStreaming.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
-download_install_mega_module_on_the_fly megaKodiWithStreaming ~ 72580038 'https://mega.co.nz/#!R0ZSFA6A!qQSeN7azchuK23RJktIMpt6l_FH2Ow0smhThUHtGg5o'
+download_install_mega_module_on_the_fly megaKodiWithStreaming ~ 337274247 'https://mega.co.nz/#!E5xXjYKZ!DFn8oxXuSO9G603_PXO0Rxhgnz5FDRGueo_zwDMS48U'
 reset_permissions_bios_and_roms
