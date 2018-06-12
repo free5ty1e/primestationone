2b3334da6c3ff0713503f8d6752f2425aee27e55
Chris Paiano <chris.paiano@gmail.com>
2018-06-12 07:23:08 -0700

    New fba mega module

---
+++ RetroPie/roms/settings/megaModules/install_mega_module_fba_roms.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+megaInstallFbaRoms.sh
---
+++ bin/megaInstallFbaRoms.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
+download_install_mega_module_on_the_fly megaFbaRoms ~ 14394679014 'https://mega.co.nz/#!rb5yhAoK!0GYxc0OI_vxmmB10izhKONqvpOYlcB6-aL_oOVs_XBo'
+reset_permissions_bios_and_roms
