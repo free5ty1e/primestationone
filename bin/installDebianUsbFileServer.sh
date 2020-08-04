35098577dea54f39ca3f1eb5be502cfeafabc75c
Chris Paiano <chris.paiano@gmail.com>
2020-08-03 18:53:31 -0700

    Created script to install my Debian USB File Server

---
+++ bin/installDebianUsbFileServer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing free5ty1e's (hey, that's me!) Debian USB File Server repository"
+
+sudo apt-get update
+sudo apt-get -y install git
+pushd ~
+rm -rf debianusbfileserve
+git clone https://github.com/free5ty1e/debianusbfileserver.git
+debianusbfileserver/scripts/installfiles.sh
+popd
+finishinstall.sh
