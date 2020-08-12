44ed85effdbf4d60a4cdcaac06ca3716486ac390
Chris Paiano <chris.paiano@gmail.com>
2020-08-11 20:49:43 -0700

    Fixed apt package install script

--- bin/installAptRuntimePackages.sh
+++ bin/installAptRuntimePackages.sh
@@ -5,8 +5,9 @@ sudo apt-get clean
 sudo dpkg --configure -a
 sudo apt-get -yf install
 
-echo Obtaining runtime packages / dependencies required for the basics for just running PrimeStationOne...
-sudo apt-get install -y git dialog joystick ntfs-3g cowsay imagemagick angband pv bc libssl-dev glib-networking expect openssl gdisk rsync libav-tools zip unzip
+echo "Obtaining runtime packages / dependencies required for the basics for just running PrimeStationOne..."
+sudo apt-get install -y git dialog joystick ntfs-3g cowsay imagemagick angband pv bc libssl-dev glib-networking expect openssl gdisk rsync zip unzip
+sudo apt-get install -y libav-tools
 
 installGoAndAnsize.sh
 #libbluetooth3 bluez bluez-utils bluez-compat bluez-hcidump bluetooth wicd-curses
