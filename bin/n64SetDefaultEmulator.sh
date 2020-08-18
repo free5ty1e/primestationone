35d9245c5f7fc992082bac7a09e0fa00e4768073
Chris Paiano <chris.paiano@gmail.com>
2020-08-17 20:46:55 -0700

    Default n64 emu to mupen64 glide

--- bin/n64SetDefaultEmulator.sh
+++ bin/n64SetDefaultEmulator.sh
@@ -2,7 +2,6 @@
 echo "Setting N64 default emulator..."
 source "/home/pi/RetroPie-Setup/scriptmodules/helpers.sh"
 source "/home/pi/RetroPie-Setup/scriptmodules/inifuncs.sh"
-iniConfig " = " "" "/opt/retropie/configs/n64/emulators.cfg"
+iniConfig ' = ' '"' "/opt/retropie/configs/n64/emulators.cfg"
 
-
-iniSet "default" "\"lr-glupen64\""
+iniSet "default" "mupen64plus-GLideN64"
