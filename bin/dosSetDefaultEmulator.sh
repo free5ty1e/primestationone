4c38fc10a3caa0be51100e13a9173cfdc117b289
Chris Paiano <chris.paiano@gmail.com>
2020-08-17 21:21:30 -0700

    Fixed dos location

--- bin/dosSetDefaultEmulator.sh
+++ bin/dosSetDefaultEmulator.sh
@@ -2,6 +2,6 @@
 echo "Setting DOS default emulator..."
 source "/home/pi/RetroPie-Setup/scriptmodules/helpers.sh"
 source "/home/pi/RetroPie-Setup/scriptmodules/inifuncs.sh"
-iniConfig ' = ' '"' "/opt/retropie/configs/dos/emulators.cfg"
+iniConfig ' = ' '"' "/opt/retropie/configs/pc/emulators.cfg"
 
 iniSet "default" "dosbox-sdl2"
