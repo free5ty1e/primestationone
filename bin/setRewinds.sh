924b8529f937d31e588bd699d3b410b08d882ce1
Chris Paiano <chris.paiano@gmail.com>
2020-08-23 16:45:42 -0700

    Try atari 800 rewind

--- bin/setRewinds.sh
+++ bin/setRewinds.sh
@@ -2,6 +2,7 @@
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
 
 fancy_console_message "Enabling rewind functionality on basic emulators..."
+rewindAtari800Enable.sh
 rewindAtari2600Enable.sh
 rewindGenesisEnable.sh
 rewindNesEnable.sh
