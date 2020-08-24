c5746742d835f20004b0ccac849a92864ae96832
Chris Paiano <chris.paiano@gmail.com>
2020-08-23 17:03:37 -0700

    intellivision in mapped cores, try rewind

---
+++ bin/rewindIntellivisionEnable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+message="Enabling intellivision emulation rewind feature which may cause massive slowdown just being on..."
+echo "$message"
+cowsay -f eyes "$message"
+
+source "/home/pi/RetroPie-Setup/scriptmodules/helpers.sh"
+source "/home/pi/RetroPie-Setup/scriptmodules/inifuncs.sh"
+iniConfig " = " "" "/opt/retropie/configs/intellivision/retroarch.cfg"
+iniSet "rewind_enable" "true"
