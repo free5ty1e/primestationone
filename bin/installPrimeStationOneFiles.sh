8095d1a08394395461ef9a6f44c76432ea40833f
Chris Paiano <chris.paiano@gmail.com>
2020-10-12 22:29:32 -0700

    Fixed xroar coco joystick controls

--- bin/installPrimeStationOneFiles.sh
+++ bin/installPrimeStationOneFiles.sh
@@ -24,6 +24,7 @@ mkdir -p ~/RetroPie/roms/mame/mame2003/cfg
 cp -vr RetroPie/* ~/RetroPie/
 cp -vr reference/.atari800.cfg ~/
 cp -vr ~/RetroPie/BIOS/dc_*.bin ~/RetroPie/BIOS/dc/
+ln --symbolic /opt/retropie/configs/coco/xroar.conf .xroar/xroar.conf
 
 # echo "Installing auto-expand check script..."
 # sudo cp -fv /home/pi/primestationone/bin/fsExpandCheck_jessie.sh /etc/init.d/
