60d3cb8ec7ec978ecae8e83682df53344c91bc49
Chris Paiano <chris.paiano@gmail.com>
2020-11-26 15:00:31 -0800

    Added tree to runtime packages

--- bin/installAptRuntimePackages.sh
+++ bin/installAptRuntimePackages.sh
@@ -6,7 +6,7 @@ sudo dpkg --configure -a
 sudo apt-get -yf install
 
 echo "Obtaining runtime packages / dependencies required for the basics for just running PrimeStationOne..."
-sudo apt-get install -y git dialog joystick ntfs-3g cowsay imagemagick angband pv bc libssl-dev glib-networking expect openssl gdisk rsync zip unzip
+sudo apt-get install -y git dialog joystick ntfs-3g cowsay imagemagick angband pv bc libssl-dev glib-networking expect openssl gdisk rsync zip unzip tree
 sudo apt-get install -y libav-tools
 
 installGoAndAnsize.sh
