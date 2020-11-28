78a867dc1f843360db988420c166930fa9ab0467
Chris Paiano <chris.paiano@gmail.com>
2020-11-27 23:40:16 -0800

    Updated binsnroms archive

--- bin/megaInstallBinsNRoms.sh
+++ bin/megaInstallBinsNRoms.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 source "/home/pi/primestationone/reference/lib/primestation_bash_functions.sh"
-download_install_mega_module_on_the_fly binsnroms ~ 212394829 'https://mega.nz/JsQVkAbS#Nyg5mpJn-BLYrONVa3bqfY6FeKtmCeIPHwSMZb-lnFA' 0 0
+download_install_mega_module_on_the_fly binsnroms ~ 814387081 'https://mega.nz/file/I1wwzIJC#AMl_QBboPd_sPmtiXIW5NfycFxwcms_Akxnvu7uzaO0' 0 0
 reset_permissions_bios_and_roms
 
-cd ~/archive
-rm -rf uae4all
-p7zip -d uae4all.7z
-mkdir ~/RetroPie/emulators
-cp -rv uae4all ~/RetroPie/emulators/
+# cd ~/archive
+# rm -rf uae4all
+# p7zip -d uae4all.7z
+# mkdir ~/RetroPie/emulators
+# cp -rv uae4all ~/RetroPie/emulators/
 
 quickCreateFoldersAndLinksAndRemoveOldFiles.sh
