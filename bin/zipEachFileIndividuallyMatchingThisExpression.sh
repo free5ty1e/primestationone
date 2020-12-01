426b74d168bbd4456a2de42a571888b3653566d9
Chris Paiano <chris.paiano@gmail.com>
2020-11-30 19:57:57 -0800

    zipEach script now uses best compression

--- bin/zipEachFileIndividuallyMatchingThisExpression.sh
+++ bin/zipEachFileIndividuallyMatchingThisExpression.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Finding each file matching $1 in current folder and zipping individually"
-find . -name "*$1*" -print -exec zip '{}'.zip '{}' \;
+find . -name "*$1*" -print -exec zip '{}'.zip -9 '{}' \;
