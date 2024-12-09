aa7d97fcc21cb07ed0ac5a3d64da24511cf3d940
Chris Paiano <chris.paiano@gmail.com>
2024-12-09 14:29:12 -0500

    New install script for generic ps3 diswoe driver

---
+++ bin/installPs3GenericDiswoeDriver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Adding support for stubborn 3rd party ps3 controllers to pair via bluetooth..."
+
+sudo ~/RetroPie-Setup/retropie_packages.sh "sixaxis"
+sudo cleanupTempFiles.sh
