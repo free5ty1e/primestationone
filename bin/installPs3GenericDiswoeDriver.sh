d870ef9d024cd513f359b6e9a20a9de95d213d16
Chris Paiano <chris.paiano@gmail.com>
2024-12-09 14:35:35 -0500

    Added customhidsony to packages list for 3rd party controller support

--- bin/installPs3GenericDiswoeDriver.sh
+++ bin/installPs3GenericDiswoeDriver.sh
@@ -3,4 +3,6 @@
 echo "Adding support for stubborn 3rd party ps3 controllers to pair via bluetooth..."
 
 sudo ~/RetroPie-Setup/retropie_packages.sh "sixaxis"
+sudo ~/RetroPie-Setup/retropie_packages.sh "customhidsony"
+
 sudo cleanupTempFiles.sh
