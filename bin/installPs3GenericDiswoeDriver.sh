ea4b0660b3f546134d0601bd7619790374447b32
Chris Paiano <chris.paiano@gmail.com>
2024-12-09 23:34:42 -0500

    Install ps3 generic diswoe driver and custom ps3diswoepair.c from sixpair.c

--- bin/installPs3GenericDiswoeDriver.sh
+++ bin/installPs3GenericDiswoeDriver.sh
@@ -2,7 +2,18 @@
 
 echo "Adding support for stubborn 3rd party ps3 controllers to pair via bluetooth..."
 
-sudo ~/RetroPie-Setup/retropie_packages.sh "sixaxis"
-sudo ~/RetroPie-Setup/retropie_packages.sh "customhidsony"
+# sudo ~/RetroPie-Setup/retropie_packages.sh "sixaxis"
+# sudo ~/RetroPie-Setup/retropie_packages.sh "customhidsony"
 
-sudo cleanupTempFiles.sh
+#Sixpair to set bluetooth master for USB-connected ps3 controller
+# sudo ~/RetroPie-Setup/retropie_packages.sh "ps3controller"
+# sudo cleanupTempFiles.sh
+sudo apt-get install bluez-utils bluez-compat bluez-hcidump checkinstall libusb-dev libbluetooth-dev joystick
+sudo apt-get -y remove cups
+sudo apt-get -y autoremove
+# wget http://www.pabr.org/sixlinux/sixpair.c
+cp ~/primestationone/reference/ps3controller/ps3DiswoePair.c ~/
+push ~
+gcc -o ps3DiswoePair ps3DiswoePair.c -lusb
+sudo cp -v ps3DiswoePair /usr/local/bin/
+popd
