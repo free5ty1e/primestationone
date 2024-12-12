6829ca4f0f5a1a0bde6e28d315174af58463440a
Chris Paiano <chris.paiano@gmail.com>
2024-12-11 20:27:59 -0500

    pipx for ps4 driver

--- bin/ps4ConnectController.sh
+++ bin/ps4ConnectController.sh
@@ -4,8 +4,8 @@ echo "Installing PS4 recommended driver to allow bluetooth pairing... ensure PS4
 echo "Reference https://retropie.org.uk/docs/PS4-Controller/ "
 
 sudo apt update
-sudo apt install python3-dev python3-pip
-sudo pip3 install ds4drv
+sudo apt install python3-dev python3-pip pipx
+sudo pipx install ds4drv
 
 sudo wget https://raw.githubusercontent.com/chrippa/ds4drv/master/udev/50-ds4drv.rules -O /etc/udev/rules.d/50-ds4drv.rules
 sudo udevadm control --reload-rules
