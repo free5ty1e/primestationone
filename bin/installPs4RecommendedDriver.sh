b27121f83161ec2e9309ae41bb32a25d39872fdb
Chris Paiano <chris.paiano@gmail.com>
2024-12-11 20:30:11 -0500

    pipx ensurepath

--- bin/installPs4RecommendedDriver.sh
+++ bin/installPs4RecommendedDriver.sh
@@ -6,6 +6,7 @@ echo "Reference https://retropie.org.uk/docs/PS4-Controller/ "
 sudo apt update
 sudo apt install python3-dev python3-pip pipx
 sudo pipx install ds4drv
+pipx ensurepath
 
 sudo wget https://raw.githubusercontent.com/chrippa/ds4drv/master/udev/50-ds4drv.rules -O /etc/udev/rules.d/50-ds4drv.rules
 sudo udevadm control --reload-rules
