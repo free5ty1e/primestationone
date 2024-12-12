8708e46499a8df2e39dc1b7d06812f08d4398ee6
Chris Paiano <chris.paiano@gmail.com>
2024-12-12 18:46:42 -0500

    iterating over controller interfaces

--- reference/ps4controller/sixpairps4.c
+++ reference/ps4controller/sixpairps4.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -17,75 +18,102 @@ void fatal(char *msg) {
 
 int main(int argc, char *argv[]) {
     libusb_device_handle *dev;
-    int res;
+    libusb_device **dev_list;
+    libusb_context *context = NULL;
+    int res, i;
+    ssize_t cnt;
     unsigned char data[16];
 
     printf("Searching for Sony DualShock 4 controller...\n");
 
     // Initialize libusb
-    res = libusb_init(NULL);
+    res = libusb_init(&context);
     if (res < 0) {
         fatal("libusb initialization failed");
     }
 
     libusb_set_option(NULL, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_DEBUG);
 
-    // Open the DualShock 4 controller (either old or new model)
-    dev = libusb_open_device_with_vid_pid(NULL, SONY_VENDOR_ID, DUALSHOCK4_PRODUCT_ID_OLD);
-    if (!dev) {
-        dev = libusb_open_device_with_vid_pid(NULL, SONY_VENDOR_ID, DUALSHOCK4_PRODUCT_ID_NEW);
-    }
-    if (!dev) {
-        fatal("No compatible DualShock 4 controller found. Is it connected via USB?");
+    // Get the list of USB devices
+    cnt = libusb_get_device_list(context, &dev_list);
+    if (cnt < 0) {
+        fatal("Failed to get device list");
     }
 
-    printf("DualShock 4 controller found!\n");
-
-    // Detach kernel driver if necessary
-    // if (libusb_kernel_driver_active(dev, 0)) {
-        res = libusb_detach_kernel_driver(dev, 0);
-        if (res != 0) {
-            printf("Warning: Could not detach kernel driver (error %d)\n", res);
-        } else {
-            printf("Kernel driver detached successfully.\n");
+    // Iterate over the list of devices and try to find the DualShock 4 controller
+    for (i = 0; i < cnt; i++) {
+        struct libusb_device_descriptor desc;
+        res = libusb_get_device_descriptor(dev_list[i], &desc);
+        if (res < 0) {
+            continue;
         }
-    // }
 
-    // Claim the interface
-    res = libusb_claim_interface(dev, 0);
-    if (res < 0) {
-        fatal("Failed to claim interface");
+        if (desc.idVendor == SONY_VENDOR_ID && 
+            (desc.idProduct == DUALSHOCK4_PRODUCT_ID_OLD || desc.idProduct == DUALSHOCK4_PRODUCT_ID_NEW)) {
+
+            printf("DualShock 4 controller found!\n");
+
+            // Open the device
+            res = libusb_open(dev_list[i], &dev);
+            if (res < 0) {
+                printf("Failed to open device\n");
+                continue;
+            }
+
+            // Iterate over available interfaces
+            for (int interface_idx = 0; interface_idx < desc.bNumConfigurations; interface_idx++) {
+                struct libusb_config_descriptor *config_desc;
+                res = libusb_get_config_descriptor(dev_list[i], interface_idx, &config_desc);
+                if (res < 0) {
+                    continue;
+                }
+
+                printf("Trying interface %d\n", interface_idx);
+
+                // Try to claim the interface
+                res = libusb_claim_interface(dev, interface_idx);
+                if (res < 0) {
+                    printf("Failed to claim interface %d (error %d)\n", interface_idx, res);
+                    continue;
+                }
+
+                // Prepare the packet with the Raspberry Pi's Bluetooth MAC address
+                unsigned char bt_mac[6] = {0x00, 0x1A, 0x7D, 0xDA, 0x71, 0x13}; // Replace with your Pi's MAC
+                memset(data, 0, sizeof(data));
+                data[0] = 0x13;  // Command identifier
+                data[1] = 0x37;  // Magic number (example, might need to adjust)
+                memcpy(&data[2], bt_mac, 6);
+
+                printf("Setting Bluetooth master to %02X:%02X:%02X:%02X:%02X:%02X\n",
+                       bt_mac[0], bt_mac[1], bt_mac[2], bt_mac[3], bt_mac[4], bt_mac[5]);
+
+                // Send the packet
+                res = libusb_control_transfer(dev,
+                                              LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE,
+                                              0x09,  // HID SET_REPORT
+                                              (0x03 << 8) | 0xF4,  // Report Type (Feature) and Report ID
+                                              0,                   // Interface
+                                              data,
+                                              sizeof(data),
+                                              0);
+                if (res < 0) {
+                    printf("Failed to send control transfer (interface %d)\n", interface_idx);
+                } else {
+                    printf("Bluetooth master address set successfully on interface %d!\n", interface_idx);
+                    libusb_release_interface(dev, interface_idx);
+                    break;  // Exit after successful transfer
+                }
+            }
+
+            // Close the device and break out of the loop
+            libusb_close(dev);
+            break;
+        }
     }
 
-    // Prepare the packet with the Raspberry Pi's Bluetooth MAC address
-    unsigned char bt_mac[6] = {0x00, 0x1A, 0x7D, 0xDA, 0x71, 0x13}; // Replace with your Pi's MAC
-    memset(data, 0, sizeof(data));
-    data[0] = 0x13;  // Command identifier
-    data[1] = 0x37;  // Magic number (example, might need to adjust)
-    memcpy(&data[2], bt_mac, 6);
-
-    printf("Setting Bluetooth master to %02X:%02X:%02X:%02X:%02X:%02X\n",
-           bt_mac[0], bt_mac[1], bt_mac[2], bt_mac[3], bt_mac[4], bt_mac[5]);
-
-    // Send the packet
-    res = libusb_control_transfer(dev,
-                                  LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE,
-                                  0x09,  // HID SET_REPORT
-                                  (0x03 << 8) | 0xF4,  // Report Type (Feature) and Report ID
-                                  0,                   // Interface
-                                  data,
-                                  sizeof(data),
-                                  5000);
-    if (res < 0) {
-        fatal("Failed to send control transfer");
-    }
-
-    printf("Bluetooth master address set successfully!\n");
-
-    // Release and close the device
-    libusb_release_interface(dev, 0);
-    libusb_close(dev);
-    libusb_exit(NULL);
+    // Clean up
+    libusb_free_device_list(dev_list, 1);
+    libusb_exit(context);
 
     return 0;
 }
