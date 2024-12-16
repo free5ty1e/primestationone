b5471d9cdd3776d3ee74698f04df805c6b4eab4d
Chris Paiano <chris.paiano@gmail.com>
2024-12-15 21:16:29 -0500

    Now stored ds4drv rules in repo

--- bin/installPs4RecommendedDriver.sh
+++ bin/installPs4RecommendedDriver.sh
@@ -37,7 +37,8 @@ sudo apt update
 sudo apt install python3-dev python3-pip
 sudo pip3 install ds4drv
 
-sudo wget https://raw.githubusercontent.com/chrippa/ds4drv/master/udev/50-ds4drv.rules -O /etc/udev/rules.d/50-ds4drv.rules
+# sudo wget https://raw.githubusercontent.com/chrippa/ds4drv/master/udev/50-ds4drv.rules -O /etc/udev/rules.d/50-ds4drv.rules
+sudo cp -v primestationone/reference/etc/udev/rules.d/50-ds4drv.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 sudo udevadm trigger
 
