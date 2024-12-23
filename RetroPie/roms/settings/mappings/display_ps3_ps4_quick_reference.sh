0dae41ffd6c5e848f2394677238e84d855ea806f
Chris Paiano <chris.paiano@gmail.com>
2024-12-22 20:52:30 -0500

    New mapping view script for emulationstation to show new ps3ps4 controls

---
+++ RetroPie/roms/settings/mappings/display_ps3_ps4_quick_reference.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#/opt/retropie/supplementary/runcommand/runcommand.sh 0 splashscreenQuickReference.sh
+displaySplashscreenWithTimeout.sh 20 "/home/pi/splashscreenps3ps4withcontrolsandversion.png"
+sleep 20
